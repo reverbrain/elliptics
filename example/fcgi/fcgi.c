0f744fbb28bb49ba4847e69326e4ccde24dd52a0
Evgeniy Polyakov <zbr@ioremap.net>
2009-11-20 19:50:49 +0300

    Basic authentification protocol implementation.

--- example/fcgi/fcgi.c
+++ example/fcgi/fcgi.c
@@ -1,3 +1,5 @@
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -5,6 +7,8 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <pthread.h>
+#include <fcntl.h>
+
 
 #include "dnet/packet.h"
 #include "dnet/interface.h"
@@ -19,13 +23,18 @@
 #define __unused	__attribute__ ((unused))
 #endif
 
-#define FCGI_DNET_ID_PATTERN		"id="
-#define FCGI_DNET_ID_DELIMITER		"&"
-#define FCGI_DNET_LOG			"/tmp/dnet_fcgi.log"
-#define FCGI_DNET_LOCAL_ADDR		"0.0.0.0:1025:2"
-#define FCGI_DNET_SUCCESS_STATUS_PATTERN	"Status: 301"
-#define FCGI_DNET_ROOT_PATTERN		""
+#define DNET_FCGI_ID_PATTERN		"id="
+#define DNET_FCGI_ID_DELIMITER		"&"
+#define DNET_FCGI_LOG			"/tmp/dnet_fcgi.log"
+#define DNET_FCGI_LOCAL_ADDR		"0.0.0.0:1025:2"
+#define DNET_FCGI_SUCCESS_STATUS_PATTERN	"Status: 301"
+#define DNET_FCGI_ROOT_PATTERN		""
 #define DNET_FCGI_MAX_REQUEST_SIZE	(100*1024*1024)
+#define DNET_FCGI_COOKIE_HEADER		"HTTP_COOKIE"
+#define DNET_FCGI_SIGN_HASH		"md5"
+#define DNET_FCGI_RANDOM_FILE		"/dev/urandom"
+#define DNET_FCGI_COOKIE_DELIMITER	"obscure_cookie="
+#define DNET_FCGI_COOKIE_ENDING		";"
 
 static FILE *dnet_fcgi_log;
 static pthread_cond_t dnet_fcgi_cond = PTHREAD_COND_INITIALIZER;
@@ -35,6 +44,23 @@ static char *dnet_fcgi_status_pattern, *dnet_fcgi_root_pattern;
 static unsigned long dnet_fcgi_max_request_size;
 static int dnet_fcgi_base_port;
 static unsigned char dnet_fcgi_id[DNET_ID_SIZE];
+
+/*
+ * This is actually not a good idea, but it will work, since
+ * no available crypto engines support that large digests.
+ */
+static char dnet_fcgi_sign_data[256];
+static char dnet_fcgi_sign_tmp[4096];
+
+/*
+ * Freaking secure long lived key...
+ */
+static char *dnet_fcgi_sign_key;
+static struct dnet_crypto_engine dnet_fcgi_sign_hash;
+
+static char *dnet_fcgi_cookie_header, *dnet_fcgi_cookie_delimiter, *dnet_fcgi_cookie_ending;
+static long dnet_fcgi_expiration_interval;
+static int dnet_urandom_fd;
 
 static int dnet_fcgi_fill_config(struct dnet_config *cfg)
 {
@@ -54,24 +80,24 @@ static int dnet_fcgi_fill_config(struct dnet_config *cfg)
 	cfg->log_private = dnet_fcgi_log;
 	cfg->log_mask = DNET_LOG_ERROR | DNET_LOG_INFO;
 
-	p = getenv("FCGI_DNET_NODE_ID");
+	p = getenv("DNET_FCGI_NODE_ID");
 	if (p) {
 		err = dnet_parse_numeric_id(p, cfg->id);
 		if (err)
 			return err;
 	}
 
-	p = getenv("FCGI_DNET_NODE_LOG_MASK");
+	p = getenv("DNET_FCGI_NODE_LOG_MASK");
 	if (p)
 		cfg->log_mask = strtoul(p, NULL, 0);
 
-	p = getenv("FCGI_DNET_NODE_WAIT_TIMEOUT");
+	p = getenv("DNET_FCGI_NODE_WAIT_TIMEOUT");
 	if (p)
 		cfg->wait_timeout = strtoul(p, NULL, 0);
 
-	p = getenv("FCGI_DNET_NODE_LOCAL_ADDR");
+	p = getenv("DNET_FCGI_NODE_LOCAL_ADDR");
 	if (!p)
-		p = FCGI_DNET_LOCAL_ADDR;
+		p = DNET_FCGI_LOCAL_ADDR;
 
 	snprintf(addr, sizeof(addr), "%s", p);
 
@@ -89,7 +115,7 @@ static int dnet_fcgi_add_remote_addr(struct dnet_node *n, struct dnet_config *ma
 	int added = 0, err;
 	struct dnet_config cfg;
 
-	addr = getenv("FCGI_DNET_REMOTE_ADDR");
+	addr = getenv("DNET_FCGI_REMOTE_ADDR");
 	if (!addr) {
 		fprintf(dnet_fcgi_log, "No remote address specified, aborting.\n");
 		err = -ENOENT;
@@ -155,7 +181,7 @@ static int dnet_fcgi_add_transform(struct dnet_node *n)
 	int added = 0, err;
 	struct dnet_crypto_engine *e;
 
-	hash = getenv("FCGI_DNET_HASH");
+	hash = getenv("DNET_FCGI_HASH");
 	if (!hash) {
 		fprintf(dnet_fcgi_log, "No hashes specified, aborting.\n");
 		err = -ENODEV;
@@ -185,7 +211,8 @@ static int dnet_fcgi_add_transform(struct dnet_node *n)
 
 		err = dnet_crypto_engine_init(e, hash);
 		if (err) {
-			fprintf(dnet_fcgi_log, "Failed to initialize hash '%s': %d.\n", hash, err);
+			fprintf(dnet_fcgi_log, "Failed to initialize hash '%s': %d.\n",
+					hash, err);
 			goto err_out_exit;
 		}
 
@@ -236,6 +263,93 @@ static void dnet_fcgi_wakeup(int err)
 	pthread_mutex_unlock(&dnet_fcgi_wait_lock);
 }
 
+static void dnet_fcgi_data_to_hex(char *dst, unsigned int dlen, unsigned char *src, unsigned int slen)
+{
+	unsigned int i;
+
+	if (slen > dlen/2 - 1)
+		slen = dlen/2 - 1;
+
+	for (i=0; i<slen; ++i)
+		sprintf(&dst[2*i], "%02x", src[i]);
+}
+
+static int dnet_fcgi_generate_sign(long timestamp)
+{
+	char *cookie = getenv(dnet_fcgi_cookie_header);
+	struct dnet_crypto_engine *e = &dnet_fcgi_sign_hash;
+	int err, len;
+	char cookie_res[128];
+	unsigned int rsize = sizeof(dnet_fcgi_sign_data);
+
+	if (cookie) {
+		char *val, *end;
+
+		val = strstr(cookie, dnet_fcgi_cookie_delimiter);
+		cookie = NULL;
+
+		if (val) {
+			end = strstr(val, dnet_fcgi_cookie_ending);
+
+			len = end - val;
+			if (len > (int)sizeof(cookie_res) - 1)
+				len = sizeof(cookie_res) - 1;
+
+			snprintf(cookie_res, len, "%s", val);
+			fprintf(dnet_fcgi_log, "Using presented cookie: '%s'.\n", cookie_res);
+			cookie = cookie_res;
+		}
+	}
+
+	if (!cookie) {
+		unsigned long long tmp;
+		char *addr = getenv("REMOTE_ADDR");
+
+		err = read(dnet_urandom_fd, &tmp, sizeof(tmp));
+		if (err < 0) {
+			err = -errno;
+			fprintf(dnet_fcgi_log, "%s: failed to read random data: %s [%d].\n",
+					addr, strerror(errno), errno);
+			goto err_out_exit;
+		}
+
+		cookie = dnet_fcgi_sign_tmp;
+		len = snprintf(dnet_fcgi_sign_tmp, sizeof(dnet_fcgi_sign_tmp), "%s-%lx-%llx", addr, timestamp, tmp);
+
+		e->init(e, NULL);
+		e->update(e, dnet_fcgi_sign_tmp, len, dnet_fcgi_sign_data, &rsize, 0);
+		e->final(e, dnet_fcgi_sign_data, dnet_fcgi_sign_data, &rsize, 0);
+
+		dnet_fcgi_data_to_hex(cookie_res, sizeof(cookie_res), (unsigned char *)dnet_fcgi_sign_data, rsize);
+
+		FCGI_printf("Set-Cookie: %s%s", dnet_fcgi_cookie_delimiter, cookie_res);
+		if (dnet_fcgi_expiration_interval) {
+			char str[128];
+			struct tm tm;
+			time_t t = timestamp + dnet_fcgi_expiration_interval;
+
+			localtime_r(&t, &tm);
+			strftime(str, sizeof(str), "%a, %d-%b-%Y %T %Z", &tm);
+			FCGI_printf("%s expires=%s", dnet_fcgi_cookie_ending, str);
+		}
+		FCGI_printf("\r\n");
+		fprintf(dnet_fcgi_log, "Using 1: %s\n", cookie_res);
+	}
+
+	err = 0;
+	len = snprintf(dnet_fcgi_sign_tmp, sizeof(dnet_fcgi_sign_tmp), "%s%lx%s", dnet_fcgi_sign_key, timestamp, cookie_res);
+
+	rsize = sizeof(dnet_fcgi_sign_data);
+	e->init(e, NULL);
+	e->update(e, dnet_fcgi_sign_tmp, len, dnet_fcgi_sign_data, &rsize, 0);
+	e->final(e, dnet_fcgi_sign_data, dnet_fcgi_sign_data, &rsize, 0);
+	
+	dnet_fcgi_data_to_hex(dnet_fcgi_sign_tmp, sizeof(dnet_fcgi_sign_tmp), (unsigned char *)dnet_fcgi_sign_data, rsize);
+
+err_out_exit:
+	return err;
+}
+
 static int dnet_fcgi_lookup_complete(struct dnet_net_state *st, struct dnet_cmd *cmd,
 		struct dnet_attr *attr, void *priv)
 {
@@ -263,8 +377,15 @@ static int dnet_fcgi_lookup_complete(struct dnet_net_state *st, struct dnet_cmd 
 		if (attr->flags) {
 			char id[DNET_ID_SIZE*2+1];
 			int port = dnet_server_convert_port((struct sockaddr *)a->addr.addr, a->addr.addr_len);
+			long timestamp = time(NULL);
 
 			snprintf(id, sizeof(id), "%s", dnet_dump_id_len(dnet_fcgi_id, DNET_ID_SIZE));
+
+			fprintf(dnet_fcgi_log, "%s -> http://%s%s/%02x/%s\n",
+					dnet_fcgi_status_pattern,
+					dnet_state_dump_addr_only(&a->addr),
+					dnet_fcgi_root_pattern,
+					dnet_fcgi_id[0], id);
 
 			FCGI_printf("%s\r\n", dnet_fcgi_status_pattern);
 			FCGI_printf("Location: http://%s%s/%d/%02x/%s\r\n",
@@ -273,20 +394,23 @@ static int dnet_fcgi_lookup_complete(struct dnet_net_state *st, struct dnet_cmd 
 					port - dnet_fcgi_base_port,
 					dnet_fcgi_id[0], id);
 
+			if (dnet_fcgi_sign_key) {
+				err = dnet_fcgi_generate_sign(timestamp);
+				if (err)
+					goto err_out_exit;
+			}
+
 			FCGI_printf("Content-type: application/xml\r\n");
 			FCGI_printf("\r\n\r\n");
-			FCGI_printf("<data id=\"%s\" url=\"http://%s%s/%d/%02x/%s\" timestamp=\"%llu\"/>", id,
-					dnet_state_dump_addr_only(&a->addr),
-					dnet_fcgi_root_pattern,
-					port - dnet_fcgi_base_port,
-					dnet_fcgi_id[0], id,
-					time(NULL));
 
-			fprintf(dnet_fcgi_log, "%s -> http://%s%s/%02x/%s\n",
-					dnet_fcgi_status_pattern,
+			FCGI_printf("<download-info><host>%s</host><path>%s/%d/%02x/%s</path><ts>%lx</ts>",
 					dnet_state_dump_addr_only(&a->addr),
-					dnet_fcgi_root_pattern,
-					dnet_fcgi_id[0], id);
+					dnet_fcgi_root_pattern, port - dnet_fcgi_base_port, dnet_fcgi_id[0], id,
+					timestamp);
+			if (dnet_fcgi_sign_key)
+				FCGI_printf("<s>%s</s>", dnet_fcgi_sign_tmp);
+			FCGI_printf("</download-info>\r\n");
+
 			err = 0;
 		}
 
@@ -473,6 +597,92 @@ err_out_exit:
 	return -EINVAL;
 }
 
+static void dnet_fcgi_destroy_sign_hash(void)
+{
+	if (!dnet_fcgi_sign_key)
+		return;
+
+	close(dnet_urandom_fd);
+	dnet_crypto_engine_exit(&dnet_fcgi_sign_hash);
+}
+
+static int dnet_fcgi_setup_sign_hash(void)
+{
+	char *p;
+	int err;
+
+	dnet_fcgi_sign_key = getenv("DNET_FCGI_SIGN_KEY");
+	if (!dnet_fcgi_sign_key) {
+		err = 0;
+		fprintf(dnet_fcgi_log, "No sign key, system will not authentificate users.\n");
+		goto err_out_exit;
+	}
+
+	p = getenv("DNET_FCGI_SIGN_HASH");
+	if (!p)
+		p = DNET_FCGI_SIGN_HASH;
+
+	err = dnet_crypto_engine_init(&dnet_fcgi_sign_hash, p);
+	if (err) {
+		fprintf(dnet_fcgi_log, "Failed to initialize hash '%s': %d.\n", p, err);
+		goto err_out_exit;
+	}
+
+	p = getenv("DNET_FCGI_RANDOM_FILE");
+	if (!p)
+		p = DNET_FCGI_RANDOM_FILE;
+	err = open(p, O_RDONLY);
+	if (err < 0) {
+		err = -errno;
+		fprintf(dnet_fcgi_log, "Failed to open (read-only) random file '%s': %s [%d].\n",
+				p, strerror(errno), errno);
+		goto err_out_destroy;
+	}
+	dnet_urandom_fd  = err;
+
+	dnet_fcgi_cookie_header = getenv("DNET_FCGI_COOKIE_HEADER");
+	if (!dnet_fcgi_cookie_header)
+		dnet_fcgi_cookie_header = DNET_FCGI_COOKIE_HEADER;
+
+	dnet_fcgi_cookie_delimiter = getenv("DNET_FCGI_COOKIE_DELIMITER");
+	if (!dnet_fcgi_cookie_delimiter)
+		dnet_fcgi_cookie_delimiter = DNET_FCGI_COOKIE_DELIMITER;
+
+	dnet_fcgi_cookie_ending = getenv("DNET_FCGI_COOKIE_ENDING");
+	if (!dnet_fcgi_cookie_ending)
+		dnet_fcgi_cookie_ending = DNET_FCGI_COOKIE_ENDING;
+
+	p = getenv("DNET_FCGI_COOKIE_EXPIRATION_INTERVAL");
+	if (p)
+		dnet_fcgi_expiration_interval = atoi(p);
+
+	return 0;
+
+err_out_destroy:
+	dnet_crypto_engine_exit(&dnet_fcgi_sign_hash);
+err_out_exit:
+	dnet_fcgi_sign_key = NULL;
+	return err;
+}
+
+static int dnet_fcgi_handle_get_redirect(struct dnet_node *n, char *addr, char *id, int length)
+{
+	int err;
+
+	err = dnet_fcgi_lookup(n, id, length);
+	if (err) {
+		err = -errno;
+		fprintf(dnet_fcgi_log, "%s: Failed to lookup object '%s': %d.\n", addr, id, err);
+		goto err_out_exit;
+	}
+	fflush(dnet_fcgi_log);
+
+	return 0;
+
+err_out_exit:
+	return err;
+}
+
 int main()
 {
 	char *p, *addr, *reason, *method;
@@ -482,24 +692,24 @@ int main()
 	struct dnet_config cfg;
 	struct dnet_node *n;
 
-	dnet_fcgi_status_pattern = getenv("FCGI_DNET_SUCCESS_STATUS_PATTERN");
+	dnet_fcgi_status_pattern = getenv("DNET_FCGI_SUCCESS_STATUS_PATTERN");
 	if (!dnet_fcgi_status_pattern)
-		dnet_fcgi_status_pattern = FCGI_DNET_SUCCESS_STATUS_PATTERN;
+		dnet_fcgi_status_pattern = DNET_FCGI_SUCCESS_STATUS_PATTERN;
 
-	dnet_fcgi_root_pattern = getenv("FCGI_DNET_ROOT_PATTERN");
+	dnet_fcgi_root_pattern = getenv("DNET_FCGI_ROOT_PATTERN");
 	if (!dnet_fcgi_root_pattern)
-		dnet_fcgi_root_pattern = FCGI_DNET_ROOT_PATTERN;
+		dnet_fcgi_root_pattern = DNET_FCGI_ROOT_PATTERN;
 
-	p = getenv("FCGI_DNET_MAX_REQUEST_SIZE");
+	p = getenv("DNET_FCGI_MAX_REQUEST_SIZE");
 	if (p)
 		dnet_fcgi_max_request_size = strtoul(p, NULL, 0);
 
 	if (!dnet_fcgi_max_request_size)
 		dnet_fcgi_max_request_size = DNET_FCGI_MAX_REQUEST_SIZE;
 
-	p = getenv("FCGI_DNET_LOG");
+	p = getenv("DNET_FCGI_LOG");
 	if (!p)
-		p = FCGI_DNET_LOG;
+		p = DNET_FCGI_LOG;
 
 	dnet_fcgi_log = fopen(p, "a");
 	if (!dnet_fcgi_log) {
@@ -508,10 +718,10 @@ int main()
 		goto err_out_exit;
 	}
 
-	p = getenv("FCGI_DNET_BASE_PORT");
+	p = getenv("DNET_FCGI_BASE_PORT");
 	if (!p) {
 		err = -ENOENT;
-		fprintf(dnet_fcgi_log, "No FCGI_DNET_BASE_PORT provided, I will not be able to determine proper directory to fetch objects.\n");
+		fprintf(dnet_fcgi_log, "No DNET_FCGI_BASE_PORT provided, I will not be able to determine proper directory to fetch objects.\n");
 		goto err_out_close;
 	}
 	dnet_fcgi_base_port = atoi(p);
@@ -522,9 +732,13 @@ int main()
 		goto err_out_close;
 	}
 
+	err = dnet_fcgi_setup_sign_hash();
+	if (err)
+		goto err_out_close;
+
 	n = dnet_node_create(&cfg);
 	if (!n)
-		goto err_out_close;
+		goto err_out_sign_destroy;
 
 	err = dnet_fcgi_add_remote_addr(n, &cfg);
 	if (err)
@@ -535,20 +749,20 @@ int main()
 		goto err_out_free;
 
 	post_allowed = 0;
-	p = getenv("FCGI_DNET_POST_ALLOWED");
+	p = getenv("DNET_FCGI_POST_ALLOWED");
 	if (p)
 		post_allowed = atoi(p);
 
 	fprintf(dnet_fcgi_log, "Started on %s, POST is %s.\n", getenv("SERVER_ADDR"), (post_allowed) ? "allowed" : "not allowed");
 	fflush(dnet_fcgi_log);
 
-	id_pattern = getenv("FCGI_DNET_ID_PATTERN");
-	id_delimiter = getenv("FCGI_DNET_ID_DELIMITER");
+	id_pattern = getenv("DNET_FCGI_ID_PATTERN");
+	id_delimiter = getenv("DNET_FCGI_ID_DELIMITER");
 
 	if (!id_pattern)
-		id_pattern = FCGI_DNET_ID_PATTERN;
+		id_pattern = DNET_FCGI_ID_PATTERN;
 	if (!id_delimiter)
-		id_delimiter = FCGI_DNET_ID_DELIMITER;
+		id_delimiter = DNET_FCGI_ID_DELIMITER;
 
 	id_pattern_length = strlen(id_pattern);
 
@@ -603,14 +817,12 @@ int main()
 				goto err_continue;
 			}
 		} else {
-			err = dnet_fcgi_lookup(n, id, length);
+			err = dnet_fcgi_handle_get_redirect(n, addr, id, length);
 			if (err) {
-				err = -errno;
-				fprintf(dnet_fcgi_log, "%s: Failed to lookup object '%s': %d.\n", addr, id, err);
-				reason = "failed to lookup object";
+				fprintf(dnet_fcgi_log, "%s: Failed to handle GET (redirect) for object '%s': %d.\n", addr, id, -err);
+				reason = "failed to handle GET";
 				goto err_continue;
 			}
-			fflush(dnet_fcgi_log);
 		}
 
 		FCGI_Finish();
@@ -624,6 +836,7 @@ err_continue:
 	}
 
 	dnet_node_destroy(n);
+	dnet_fcgi_destroy_sign_hash();
 	fflush(dnet_fcgi_log);
 	fclose(dnet_fcgi_log);
 
@@ -631,6 +844,8 @@ err_continue:
 
 err_out_free:
 	dnet_node_destroy(n);
+err_out_sign_destroy:
+	dnet_fcgi_destroy_sign_hash();
 err_out_close:
 	fflush(dnet_fcgi_log);
 	fclose(dnet_fcgi_log);
