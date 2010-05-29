c4e69d7bbdaecf562dda18caf6d94ac895a65987
Evgeniy Polyakov <zbr@ioremap.net>
2010-05-29 20:38:44 +0400

    Drop unused meta processing code.

--- library/meta.c
+++ library/meta.c
@@ -154,74 +154,10 @@ err_out_exit:
 	return NULL;
 }
 
-int dnet_meta_create_file(struct dnet_node *n, char *metafile, struct dnet_meta *m, void *mdata)
-{
-	int fd, err, size;
-	struct dnet_meta *meta;
-	struct stat st;
-	void *data;
-
-	fd = open(metafile, O_RDWR | O_CREAT, 0644);
-	if (fd < 0) {
-		err = -errno;
-		dnet_map_log(n, DNET_LOG_ERROR, "Failed to open metadata file '%s'", metafile);
-		goto err_out_exit;
-	}
-
-	err = fstat(fd, &st);
-	if (err) {
-		err = -errno;
-		dnet_map_log(n, DNET_LOG_ERROR, "Failed to stat metadata file '%s'", metafile);
-		goto err_out_close;
-	}
-	size = st.st_size;
-
-	data = meta = malloc(size);
-	if (!meta) {
-		err = -errno;
-		dnet_map_log(n, DNET_LOG_ERROR, "Failed to allocate %d bytes for metadata file '%s'", size, metafile);
-		goto err_out_close;
-	}
-
-	err = read(fd, meta, size);
-	if (err != size) {
-		err = -errno;
-		dnet_map_log(n, DNET_LOG_ERROR, "Failed to read %d bytes from metadata file '%s'", size, metafile);
-		goto err_out_free;
-	}
-
-	dnet_map_log(n, DNET_LOG_INFO, "%s: meta: %d, meta_size: %d, size: %d.\n", metafile, m->type, m->size, size);
-
-	meta = dnet_meta_replace(n, meta, (uint32_t *)&size, m, mdata);
-	if (!meta) {
-		err = -ENOMEM;
-		dnet_map_log(n, DNET_LOG_ERROR, "Failed to replace metadata in file '%s'", metafile);
-		goto err_out_free;
-	}
-
-	data = meta;
-
-	err = pwrite(fd, meta, size, 0);
-	if (err != size) {
-		err = -ENOMEM;
-		dnet_map_log(n, DNET_LOG_ERROR, "Failed to write metadata in file '%s'", metafile);
-		goto err_out_free;
-	}
-	err = 0;
-
-err_out_free:
-	free(data);
-err_out_close:
-	close(fd);
-err_out_exit:
-	return err;
-}
-
 int dnet_meta_read_object_id(struct dnet_node *n, unsigned char *id, char *file)
 {
 	int err, len;
 	struct dnet_io_attr io;
-	struct dnet_history_map m;
 	struct dnet_wait *w;
 	char tmp[32];
 
@@ -247,112 +183,11 @@ int dnet_meta_read_object_id(struct dnet_node *n, unsigned char *id, char *file)
 
 	snprintf(tmp, sizeof(tmp), "/tmp/meta-hist-%d%s", getpid(), DNET_HISTORY_SUFFIX);
 
-	err = dnet_map_history(n, tmp, &m);
-	if (err)
-		goto err_out_unlink;
+	unlink(file);
+	rename(tmp, file);
 
-	io.flags = 0;
-	memcpy(io.id, m.ent[m.num - 1].id, DNET_ID_SIZE);
-	memcpy(io.origin, io.id, DNET_ID_SIZE);
-
-	err = dnet_read_file_id(n, file, strlen(file), 0, 0, &io, w, 0, 1);
-	dnet_map_log(n, DNET_LOG_INFO, "%s: metadata reading transaction: %d.\n", dnet_dump_id(io.origin), err);
-	if (err)
-		goto err_out_unmap;
-
-err_out_unmap:
-	dnet_unmap_history(n, &m);
-err_out_unlink:
-	unlink(tmp);
 err_out_put:
 	dnet_wait_put(w);
-err_out_exit:
-	return err;
-}
-
-static int dnet_meta_read_object(struct dnet_node *n, char *meta_object, int meta_len, char *metafile)
-{
-	int err, pos = 0, error = 0;
-	unsigned char id[DNET_ID_SIZE];
-	unsigned char addr[DNET_ID_SIZE];
-
-	while (1) {
-		unsigned int rsize = DNET_ID_SIZE;
-
-		err = dnet_transform(n, meta_object, meta_len, id, addr, &rsize, &pos);
-		if (err) {
-			if (err > 0)
-				break;
-			if (!error)
-				error = err;
-			continue;
-		}
-
-		err = dnet_meta_read_object_id(n, id, metafile);
-		if (err) {
-			error = err;
-			continue;
-		}
-
-		error = 0;
-		break;
-	}
-
-	return error;
-}
-
-int dnet_meta_read(struct dnet_node *n, char *obj, int len, char *metafile)
-{
-	char meta_object[len + sizeof(DNET_META_SUFFIX) + 1];
-
-	snprintf(meta_object, len, "%s", obj);
-	memcpy(meta_object + len, DNET_META_SUFFIX, sizeof(DNET_META_SUFFIX));
-	meta_object[sizeof(meta_object) - 1] = '\0';
-
-	return dnet_meta_read_object(n, meta_object, sizeof(meta_object), metafile);
-}
-
-int dnet_meta_write(struct dnet_node *n, struct dnet_meta *m, void *mdata,
-		char *obj, int len, char *metafile)
-{
-	char meta_object[len + sizeof(DNET_META_SUFFIX) + 1];
-	int err, meta_unlink = 0;
-	char file[64];
-
-	snprintf(meta_object, len, "%s", obj);
-	memcpy(meta_object + len, DNET_META_SUFFIX, sizeof(DNET_META_SUFFIX));
-	meta_object[sizeof(meta_object) - 1] = '\0';
-
-	if (!metafile) {
-		snprintf(file, sizeof(file), "/tmp/meta-%d", getpid());
-		err = dnet_meta_read_object(n, meta_object, sizeof(meta_object), file);
-		if (err) {
-			if (err != -ENOENT)
-				goto err_out_exit;
-		}
-
-		err = open(file, O_RDWR | O_CREAT | O_TRUNC, 0644);
-		if (err < 0) {
-			err = -errno;
-			dnet_map_log(n, DNET_LOG_ERROR, "Failed to create empty metadata file '%s'", file);
-			goto err_out_exit;
-		}
-		close(err);
-
-		metafile = file;
-		meta_unlink = 1;
-	}
-
-	err = dnet_meta_create_file(n, metafile, m, mdata);
-	if (err)
-		goto err_out_unlink;
-
-	err = dnet_write_file_local_offset(n, metafile, meta_object, sizeof(meta_object),
-			NULL, 0, 0, 0, 0, DNET_IO_FLAGS_META | DNET_IO_FLAGS_ID_CONTENT);
-
-err_out_unlink:
-	if (meta_unlink)
-		unlink(metafile);
 err_out_exit:
 	return err;
 }
