2254458c5721d93833f8a24778b289cb7a4c4128
Evgeniy Polyakov <zbr@ioremap.net>
2010-07-26 22:09:23 +0400

    Mark states added via dnet_add_state() as reconnect-friendly.

--- library/dnet.c
+++ library/dnet.c
@@ -893,7 +893,7 @@ int dnet_add_state(struct dnet_node *n, struct dnet_config *cfg)
 	st = dnet_add_state_socket(n, &addr, s);
 	if (!st) {
 		err = -EINVAL;
-		dnet_add_reconnect_state(n, &addr, cfg->join);
+		dnet_add_reconnect_state(n, &addr, cfg->join | DNET_WANT_RECONNECT);
 		goto err_out_sock_close;
 	}
 
