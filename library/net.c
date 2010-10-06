c4e8ed3125feddfec9686d02269457b69950f106
Evgeniy Polyakov <zbr@ioremap.net>
2010-10-06 22:37:27 +0400

    Disable alloc/free logging.

--- library/net.c
+++ library/net.c
@@ -576,12 +576,12 @@ static void dnet_schedule_command(struct dnet_net_state *st)
 	st->rcv_flags = DNET_IO_CMD;
 
 	if (st->rcv_data) {
+#if 0
 		struct dnet_cmd *c = &st->rcv_cmd;
 		unsigned long long tid = c->trans & ~DNET_TRANS_REPLY;
-
 		dnet_log(st->n, DNET_LOG_NOTICE, "freed: size: %llu, trans: %llu, reply: %d, ptr: %p.\n",
 						c->size, tid, tid != c->trans, st->rcv_data);
-
+#endif
 		free(st->rcv_data);
 		st->rcv_data = NULL;
 	}
@@ -662,9 +662,10 @@ again:
 				err = -ENOMEM;
 				goto out;
 			}
-
+#if 0
 			dnet_log(n, DNET_LOG_NOTICE, "allocated: %llu, trans: %llu, reply: %d, ptr: %p.\n",
 					c->size, tid, tid != c->trans, st->rcv_data);
+#endif
 		}
 
 		st->rcv_flags &= ~DNET_IO_CMD;
