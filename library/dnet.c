9222b538cfe82f2353df0b4384deeabd4c9b9fb9
Evgeniy Polyakov <zbr@ioremap.net>
2010-11-29 20:41:15 +0300

    Write metadata for transaction histories.

--- library/dnet.c
+++ library/dnet.c
@@ -1258,6 +1258,18 @@ int dnet_write_file_local_offset(struct dnet_node *n, char *file,
 			dnet_log(n, DNET_LOG_ERROR, "Failed to write metadata for file '%s' into the storage, transactions: %d, err: %d.\n", file, trans_num, err);
 			goto err_out_close;
 		}
+
+		if (trans_num > 1 && !(ioflags & DNET_IO_FLAGS_META)) {
+			struct dnet_id raw;
+
+			dnet_setup_id(&raw, ctl.id.group_id, ctl.io.parent);
+
+			err = dnet_create_write_metadata(n, &ctl.id, remote, remote_len, n->groups, n->group_num);
+			if (err < 0) {
+				dnet_log(n, DNET_LOG_ERROR, "Failed to write history metadata for file '%s' into the storage, transactions: %d, err: %d.\n", file, trans_num, err);
+				goto err_out_close;
+			}
+		}
 	}
 
 	err = dnet_wait_event(w, w->cond == trans_num, &n->wait_ts);
