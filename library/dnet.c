dcc8ba39036b3830c9a2b6a5431690a8725d57b8
Evgeniy Polyakov <zbr@ioremap.net>
2011-03-22 22:26:59 +0300

    do not break protocol processing for del/write commands during check.

--- library/dnet.c
+++ library/dnet.c
@@ -563,8 +563,10 @@ int dnet_process_cmd_raw(struct dnet_net_state *st, struct dnet_cmd *cmd, void *
 			case DNET_CMD_WRITE:
 			case DNET_CMD_DEL:
 				if (a->cmd == DNET_CMD_DEL || a->cmd == DNET_CMD_WRITE) {
-					if (n->check_in_progress)
-						return -EINPROGRESS;
+					if (n->check_in_progress) {
+						err = -EINPROGRESS;
+						break;
+					}
 				}
 				if (a->cmd == DNET_CMD_DEL) {
 					err = dnet_db_del(n, cmd, a);
