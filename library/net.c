49e6683e844397a2e2a9517edcadee661dd9b5de
Evgeniy Polyakov <zbr@ioremap.net>
2011-04-22 01:46:59 +0400

    Temporarily disable forwarding

--- library/net.c
+++ library/net.c
@@ -689,7 +689,7 @@ int dnet_process_recv(struct dnet_net_state *st, struct dnet_io_req *r)
 			dnet_trans_put(t);
 		goto out;
 	}
-
+#if 0
 	forward_state = dnet_state_get_first(n, &cmd->id);
 	if (!forward_state || forward_state == st || forward_state == n->st ||
 			(st->rcv_cmd.flags & DNET_FLAGS_DIRECT)) {
@@ -710,7 +710,9 @@ int dnet_process_recv(struct dnet_net_state *st, struct dnet_io_req *r)
 		goto err_out_destroy;
 
 	dnet_state_put(forward_state);
-
+#else
+	dnet_process_cmd_raw(st, cmd, r->data);
+#endif
 out:
 	return 0;
 
