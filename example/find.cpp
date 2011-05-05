c7e6fc94a6b5b67f956927c714ff9ef7aeefa466
Evgeniy Polyakov <zbr@ioremap.net>
2011-05-06 03:23:16 +0400

    Added find utility.

---
+++ example/find.cpp
@@ -0,0 +1,271 @@
+/*
+ * 2011+ Copyright (c) Evgeniy Polyakov <zbr@ioremap.net>
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include "config.h"
+
+#define _XOPEN_SOURCE 600
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+
+#include <errno.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sstream>
+#include <stdexcept>
+
+#include "elliptics/interface.h"
+#include "elliptics/packet.h"
+#include "elliptics/cppdef.h"
+
+#include "common.h"
+
+class elliptics_finder : public elliptics_node {
+	public:
+		elliptics_finder(elliptics_log &log) : elliptics_node(log) {};
+		elliptics_finder(elliptics_log &log, struct dnet_config &cfg) : elliptics_node(log, cfg) {};
+		virtual ~elliptics_finder() {};
+
+		void add_remote(const char *addr);
+
+		void sync_route(void);
+
+		void parse_lookup(const std::string &ret);
+		void parse_meta(const std::string &ret);
+};
+
+void elliptics_finder::sync_route(void)
+{
+	elliptics_callback c;
+	int err;
+
+	err = dnet_request_stat(node, NULL, DNET_CMD_ROUTE_LIST, 0,
+		elliptics_callback::elliptics_complete_callback, (void *)&c);
+	if (err < 0) {
+		std::ostringstream str;
+		str << "Failed to sync route table: " << err;
+		throw std::runtime_error(str.str());
+	}
+
+	std::string ret;
+	ret = c.wait(err);
+}
+
+void elliptics_finder::add_remote(const char *addr)
+{
+	struct dnet_config rem;
+	int err;
+
+	memset(&rem, 0, sizeof(rem));
+
+	err = dnet_parse_addr((char *)addr, &rem);
+	if (err < 0) {
+		std::ostringstream str;
+		str << "Failed to parse addr: " << addr;
+		throw std::runtime_error(str.str());
+	}
+
+	elliptics_node::add_remote(rem.addr, atoi(rem.port), rem.family);
+}
+
+void elliptics_finder::parse_lookup(const std::string &ret)
+{
+	long size = ret.size();
+	void *data = (void *)ret.data();
+
+	while (size) {
+		struct dnet_addr *addr = (struct dnet_addr *)data;
+		struct dnet_cmd *cmd = (struct dnet_cmd *)(addr + 1);
+
+		if (cmd->size) {
+			struct dnet_attr *attr = (struct dnet_attr *)(cmd + 1);
+			char addr_str[128] = "no-address";
+
+			if (attr->size == sizeof(struct dnet_addr_attr)) {
+				struct dnet_addr_attr *a = (struct dnet_addr_attr *)(attr + 1);
+
+				dnet_convert_addr_attr(a);
+				dnet_server_convert_dnet_addr_raw(&a->addr, addr_str, sizeof(addr_str));
+			}
+
+			dnet_log_raw(node, DNET_LOG_INFO, "%s: FIND: %s: cmd: %s, present: %s\n",
+					dnet_dump_id(&cmd->id), addr_str,
+					dnet_cmd_string(attr->cmd), attr->flags ? "YES" : "NO");
+
+			if (attr->flags) {
+			} else {
+			}
+		} else {
+			dnet_log_raw(node, DNET_LOG_INFO, "%s: FIND: status: %d\n", dnet_dump_id(&cmd->id), cmd->status);
+		}
+
+		data = (char *)data + sizeof(struct dnet_addr) + sizeof(struct dnet_cmd) + cmd->size;
+		size -= sizeof(struct dnet_addr) + sizeof(struct dnet_cmd) + cmd->size;
+	}
+}
+
+void elliptics_finder::parse_meta(const std::string &ret)
+{
+	long size = ret.size();
+	void *data = (void *)ret.data();
+
+	while (size) {
+		struct dnet_addr *addr = (struct dnet_addr *)data;
+		struct dnet_cmd *cmd = (struct dnet_cmd *)(addr + 1);
+		char addr_str[128];
+
+		dnet_server_convert_dnet_addr_raw(addr, addr_str, sizeof(addr_str));
+
+		if (cmd->size) {
+			struct dnet_attr *attr = (struct dnet_attr *)(cmd + 1);
+
+
+			if (attr->size > sizeof(struct dnet_io_attr)) {
+				struct dnet_io_attr *io = (struct dnet_io_attr *)(attr + 1);
+
+				dnet_convert_io_attr(io);
+
+				dnet_log_raw(node, DNET_LOG_INFO, "%s: FIND: %s: cmd: %s, io size: %llu\n",
+						dnet_dump_id(&cmd->id), addr_str, dnet_cmd_string(attr->cmd),
+						(unsigned long long)io->size);
+			} else {
+			}
+		} else {
+			dnet_log_raw(node, DNET_LOG_INFO, "%s: FIND: %s: status: %d\n", dnet_dump_id(&cmd->id), addr_str, cmd->status);
+		}
+
+		data = (char *)data + sizeof(struct dnet_addr) + sizeof(struct dnet_cmd) + cmd->size;
+		size -= sizeof(struct dnet_addr) + sizeof(struct dnet_cmd) + cmd->size;
+	}
+}
+
+static __attribute__ ((noreturn)) void efinder_usage(const char *p)
+{
+	fprintf(stderr, "Usage: %s <options>\n"
+			"  -r addr:port:family            - remote node to connect\n"
+			"  -l log                         - log file\n"
+			"  -m mask                        - log mask\n"
+			"  -I id                          - object ID\n"
+			"  -h                             - this help\n"
+			, p);
+	exit(-1);
+}
+
+int main(int argc, char *argv[])
+{
+	int ch, err;
+	char *logfile = (char *)"/dev/stderr";
+	int log_mask = DNET_LOG_INFO | DNET_LOG_ERROR;
+	char *remote = NULL;
+	struct dnet_id raw;
+	struct dnet_trans_control ctl;
+
+	while ((ch = getopt(argc, argv, "r:l:m:I:h")) != -1) {
+		switch (ch) {
+			case 'r':
+				remote = optarg;
+				break;
+			case 'l':
+				logfile = optarg;
+				break;
+			case 'm':
+				log_mask = strtoul(optarg, NULL, 0);
+				break;
+			case 'I':
+				err = dnet_parse_numeric_id(optarg, raw.id);
+				if (err < 0)
+					return err;
+				break;
+			case 'h':
+			default:
+				efinder_usage(argv[0]);
+
+		}
+	}
+
+	if (!remote) {
+		fprintf(stderr, "You must specify remote addr and object ID\n");
+		efinder_usage(argv[0]);
+	}
+
+	try {
+		elliptics_log_file log(logfile, log_mask);
+		elliptics_finder find(log);
+
+		find.add_remote(remote);
+		find.sync_route();
+
+		{
+			elliptics_callback c;
+
+			memset(&ctl, 0, sizeof(struct dnet_trans_control));
+
+			ctl.priv = (void *)&c;
+			ctl.complete = elliptics_callback::elliptics_complete_callback;
+
+			dnet_setup_id(&ctl.id, 0, raw.id);
+			ctl.cflags = DNET_FLAGS_DIRECT;
+			ctl.cmd = DNET_CMD_LOOKUP;
+			ctl.aflags = DNET_ATTR_LOOKUP_STAT;
+
+			int num = find.request_cmd(ctl);
+			std::string lookup_ret = c.wait(num);
+			find.parse_lookup(lookup_ret);
+		}
+
+
+
+		{
+			elliptics_callback c;
+
+			memset(&ctl, 0, sizeof(ctl));
+
+			ctl.priv = (void *)&c;
+			ctl.complete = elliptics_callback::elliptics_complete_callback;
+
+			dnet_setup_id(&ctl.id, 0, raw.id);
+			ctl.cflags = DNET_FLAGS_DIRECT;
+			ctl.cmd = DNET_CMD_READ;
+			ctl.cflags = DNET_FLAGS_NEED_ACK;
+
+			struct dnet_io_attr io;
+			memset(&io, 0, sizeof(io));
+			io.flags = DNET_IO_FLAGS_META;
+			memcpy(io.id, ctl.id.id, DNET_ID_SIZE);
+			memcpy(io.parent, ctl.id.id, DNET_ID_SIZE);
+			ctl.data = &io;
+			ctl.size = sizeof(io);
+
+			int num = find.request_cmd(ctl);
+			std::string meta_ret = c.wait(num);
+			find.parse_meta(meta_ret);
+		}
+	} catch (const std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	return 0;
+}
+
