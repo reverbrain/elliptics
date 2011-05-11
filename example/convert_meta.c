29fe5ebeb69dc579015163eb6d9e0f15c8855635
Anton Kortunov <toshic.toshic@gmail.com>
2011-05-11 13:04:00 +0400

    Store converted meta in the new file

--- example/convert_meta.c
+++ example/convert_meta.c
@@ -36,6 +36,9 @@
 
 #include "common.h"
 
+//BUFFER_SIZE = 1MB
+#define BUFFER_SIZE 1048576
+
 static void hparser_usage(const char *p)
 {
 	fprintf(stderr, "Usage: %s args\n", p);
@@ -48,10 +51,18 @@ static void hparser_usage(const char *p)
 uint64_t counter = 0;
 uint64_t total = 0;
 
+struct db_ptrs {
+	KCDB *meta;
+	KCDB *newmeta;
+	char *buffer;
+};
+
 static const char *hparser_visit(const char *key, size_t keysz,
 			const char *hdata, size_t datasz, size_t *sp __attribute((unused)), void *opq)
 {
-	KCDB *meta = opq;
+	//KCDB *meta = opq;
+	struct db_ptrs *ptrs = opq;
+	char *data = NULL;
 	char id_str[2 * DNET_ID_SIZE + 1];
 	struct dnet_history_map hm;
 	struct dnet_meta_container mc;
@@ -74,7 +85,7 @@ static const char *hparser_visit(const char *key, size_t keysz,
 	memcpy(id, key, DNET_ID_SIZE);
 	dnet_dump_id_len_raw(id, DNET_ID_SIZE, id_str);
 	
-	fprintf(stdout, "Processing key %.128s\n", id_str);
+	fprintf(stdout, "Processing key %.128s  ", id_str);
 
 	if (datasz % (int)sizeof(struct dnet_history_entry)) {
 		fprintf(stdout, "Corrupted history record, "
@@ -88,24 +99,32 @@ static const char *hparser_visit(const char *key, size_t keysz,
 	hm.size = datasz;
 
 	dnet_setup_id(&mc.id, 0, id);
-	mc.data = (unsigned char *)kcdbget(meta, (void *)key, DNET_ID_SIZE, &mc.size);
-	if (!mc.data) {
-		err = -kcdbecode(meta);
-		fprintf(stdout, "%s: meta DB read failed "
+	data = kcdbget(ptrs->meta, (void *)key, DNET_ID_SIZE, &mc.size);
+	if (!data) {
+		err = -kcdbecode(ptrs->meta);
+		fprintf(stdout, "failed. %s: meta DB read failed "
 			"err: %d: %s.\n", dnet_dump_id_str(id),
 			err, kcecodename(-err));
 		goto err_out_exit;
 	}
 
+	if (mc.size > BUFFER_SIZE) {
+		fprintf(stdout, "failed. Meta size=%d is too big\n", mc.size);
+		goto err_out_kcfree;
+	}
+
+	mc.data = ptrs->buffer;
+	memcpy(mc.data, data, mc.size);
+
 	mp = dnet_meta_search_cust(&mc, DNET_META_GROUPS);
 	if (!mp) {
-		fprintf(stdout, "Groups was not found in meta DB\n");
+		fprintf(stdout, "failed. Groups was not found in meta DB\n");
 		goto err_out_kcfree;
 	}
 
 	dnet_convert_meta(mp);
 	if (mp->size % sizeof(int)) {
-		fprintf(stdout, "Metadata is broken: entry size %u\n", mp->size);
+		fprintf(stdout, "failed. Metadata is broken: entry size %u\n", mp->size);
 		goto err_out_kcfree;
 	}
 
@@ -116,8 +135,13 @@ static const char *hparser_visit(const char *key, size_t keysz,
 
 	mp = dnet_meta_search_cust(&mc, DNET_META_UPDATE);
 	if (!mp) {
-		fprintf(stdout, "Meta update was not found in meta DB\n");
-		mp = m = malloc(sizeof(struct dnet_meta) + sizeof(struct dnet_meta_update) * group_num);
+		if (mc.size + sizeof(struct dnet_meta) + sizeof(struct dnet_meta_update) * group_num > BUFFER_SIZE) {
+			fprintf(stdout, "failed. New meta size=%d is too big\n", 
+					mc.size + sizeof(struct dnet_meta) + sizeof(struct dnet_meta_update) * group_num);
+			goto err_out_kcfree;
+		}
+		mc.size += sizeof(struct dnet_meta) + sizeof(struct dnet_meta_update) * group_num;
+		mp = m = mc.data + mc.size;
 		m->type = DNET_META_UPDATE;
 		m->size = sizeof(struct dnet_meta_update) * group_num;
 		memset(m->data, 0, m->size);
@@ -126,7 +150,7 @@ static const char *hparser_visit(const char *key, size_t keysz,
 	}
 
 	if (mp->size % sizeof(struct dnet_meta_update)) {
-		fprintf(stdout, "Metadata is broken: entry size %u\n", mp->size);
+		fprintf(stdout, "failed. Metadata is broken: entry size %u\n", mp->size);
 		goto err_out_kcfree;
 	}
 
@@ -135,7 +159,6 @@ static const char *hparser_visit(const char *key, size_t keysz,
 	dnet_convert_meta(mp);
 
 	dnet_convert_history_entry(&hm.ent[hm.num-1]);
-	fprintf(stdout, "Last update stamp %llu %llu\n", hm.ent[hm.num-1].tsec, hm.ent[hm.num-1].tnsec);
 
 	for (i = 0; i < group_num; ++i) {
 		if (m) {
@@ -151,38 +174,40 @@ static const char *hparser_visit(const char *key, size_t keysz,
 			}
 			if (j == mu_num) {
 				fprintf(stdout, "Group %d was not found in meta DB\n", groups[i]);
-				goto err_out_kcfree;
 			}
 		}
 	}
 
-	if (m) {
-		err = kcdbbegintran(meta, 1);
+	// Commit every 1000 records.
+	if (!(counter % 1000)) {
+		if (counter > 0) {
+			kcdbendtran(ptrs->newmeta, 1);
+		}
+
+		err = kcdbbegintran(ptrs->newmeta, 1);
 		if (!err) {
-			err = -kcdbecode(meta);
+			err = -kcdbecode(ptrs->newmeta);
 			fprintf(stdout, "%s: meta DB transaction start failed "
 				"err: %d: %s.\n", dnet_dump_id_str(id),
 				err, kcecodename(-err));
 			goto err_out_kcfree;
 		}
-
-		err = kcdbappend(meta, key, DNET_ID_SIZE, (void *)m, sizeof(struct dnet_meta) + sizeof(struct dnet_meta_update) * group_num);
-		if (!err) {
-			err = -kcdbecode(meta);
-			fprintf(stdout, "%s: meta DB append failed "
-				"err: %d: %s.\n", dnet_dump_id_str(id),
-				err, kcecodename(-err));
-			goto err_out_kcfree;
-		}
-
-		kcdbendtran(meta, 1);
-
 	}
 
+	err = kcdbset(ptrs->newmeta, key, DNET_ID_SIZE, (void *)m, sizeof(struct dnet_meta) + sizeof(struct dnet_meta_update) * group_num);
+	if (!err) {
+		err = -kcdbecode(ptrs->newmeta);
+		fprintf(stdout, "%s: meta DB append failed "
+			"err: %d: %s.\n", dnet_dump_id_str(id),
+			err, kcecodename(-err));
+		goto err_out_kcfree;
+	}
+
+
+	fprintf(stdout, "ok. Last update stamp %llu %llu\n", hm.ent[hm.num-1].tsec, hm.ent[hm.num-1].tnsec);
+
 err_out_kcfree:
-	if (m)
-		free(m);
-	kcfree(mc.data);
+	kcfree(data);
 err_out_exit:
 	counter++;
 	if (!(counter % 10000)) {
@@ -198,16 +223,17 @@ err_out_exit:
 int main(int argc, char *argv[])
 {
 	int err, ch;
-	char *history_name = NULL, *meta_name = NULL;
+	char *history_name = NULL, *meta_name = NULL, *newmeta_name = NULL;
 	unsigned long long offset, size;
-	KCDB *history = NULL, *meta = NULL;
+	KCDB *history = NULL, *meta = NULL, *newmeta = NULL;
 	char tstr[64];
 	time_t t;
 	struct tm *tm;
+	struct db_ptrs ptrs;
 
 	size = offset = 0;
 
-	while ((ch = getopt(argc, argv, "M:H:h")) != -1) {
+	while ((ch = getopt(argc, argv, "M:H:N:h")) != -1) {
 		switch (ch) {
 			case 'M':
 				meta_name = optarg;
@@ -215,15 +241,20 @@ int main(int argc, char *argv[])
 			case 'H':
 				history_name = optarg;
 				break;
+			case 'N':
+				newmeta_name = optarg;
+				break;
 			case 'h':
 				hparser_usage(argv[0]);
 		}
 	}
 
-	if (!meta_name || !history_name) {
+	if (!meta_name || !history_name || !newmeta_name) {
 		fprintf(stderr, "You have to provide history and meta database to convert.\n");
 		hparser_usage(argv[0]);
 	}
+
+	memset(&ptrs, 0, sizeof(struct db_ptrs));
 
 	printf("opening %s history database\n", history_name);
 	history = kcdbnew();
@@ -235,10 +266,26 @@ int main(int argc, char *argv[])
 
 	printf("opening %s meta database\n", meta_name);
 	meta = kcdbnew();
-	err = kcdbopen(meta, meta_name, KCOWRITER);
+	err = kcdbopen(meta, meta_name, KCOREADER | KCONOREPAIR);
 	if (!err) {
 		fprintf(stderr, "Failed to open meta database '%s': %d.\n", meta_name, -kcdbecode(meta));
 		goto err_out_dbopen;
+	}
+
+	printf("opening %s new meta database\n", newmeta_name);
+	newmeta = kcdbnew();
+	err = kcdbopen(newmeta, newmeta_name, KCOWRITER | KCOCREATE);
+	if (!err) {
+		fprintf(stderr, "Failed to open meta database '%s': %d.\n", newmeta_name, -kcdbecode(newmeta));
+		goto err_out_dbopen2;
+	}
+
+	ptrs.meta = meta;
+	ptrs.newmeta = newmeta;
+	ptrs.buffer = malloc(BUFFER_SIZE);
+	if (!ptrs.buffer) {
+		fprintf(stderr, "Failed to allocate memory for buffer\n");
+		goto err_out_dbopen3;
 	}
 
 	t = time(NULL);
@@ -247,10 +294,11 @@ int main(int argc, char *argv[])
 	total = (unsigned long long)kcdbcount(history);
 	fprintf(stderr, "%s: Total %llu records in history DB\n", tstr, total);
 
-	err = kcdbiterate(history, hparser_visit, meta, 0);
+	err = kcdbiterate(history, hparser_visit, &ptrs, 0);
 	if (!err) {
 		fprintf(stderr, "Failed to iterate history database '%s': %d.\n", history_name, -kcdbecode(history));
 	}
+	kcdbendtran(newmeta, 1);
 
 	t = time(NULL);
 	tm = localtime(&t);
@@ -258,9 +306,15 @@ int main(int argc, char *argv[])
 	total = (unsigned long long)kcdbcount(history);
 	fprintf(stderr, "%s: Totally processed %llu records from history DB\n", tstr, counter);
 
+err_out_dbopen3:
+	err = kcdbclose(newmeta);
+	if (!err)
+		fprintf(stderr, "Failed to close new meta database '%s': %d.\n", newmeta_name, -kcdbecode(newmeta));
+	kcdbdel(newmeta);
+err_out_dbopen2:
 	err = kcdbclose(meta);
 	if (!err)
-		fprintf(stderr, "Failed to close meta database '%s': %d.\n", meta_name, -kcdbecode(history));
+		fprintf(stderr, "Failed to close meta database '%s': %d.\n", meta_name, -kcdbecode(meta));
 	kcdbdel(meta);
 err_out_dbopen:
 	err = kcdbclose(history);
