47ad977b0e932b732182ba7b4f2f529d16b20a26
Evgeniy Polyakov <zbr@ioremap.net>
2011-06-15 21:03:41 +0400

    Use new constructors in test.py

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from libelliptics_python import *
+import sys
 
 try:
 	log = elliptics_log_file("/dev/stderr", 15)
@@ -10,12 +11,9 @@ try:
 	n.add_groups([1,2,3])
 	n.add_remote("localhost", 1025)
 
-	id = elliptics_id()
-	id.id = [1, 2, 3, 4]
-	id.group_id = 2
-	id.version = 0
-
 	print n.lookup_addr("qwerty.xml", 5)
+
+	id = elliptics_id([1, 2, 3, 4], 2)
 
 	# write data by ID into specified group (# 2)
 	# read data from the same group (# 2)
