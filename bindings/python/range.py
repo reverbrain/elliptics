e3701bd224b0690290fe176bac67026abaaaa05c
Evgeniy Polyakov <zbr@ioremap.net>
2011-06-15 21:03:56 +0400

    Added range request example

---
+++ bindings/python/range.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from libelliptics_python import *
+
+log = elliptics_log_file("/dev/stderr", 8)
+n = elliptics_node_python(log)
+
+group = 2
+
+n.add_groups([group])
+n.add_remote("localhost", 1025)
+
+r = elliptics_range()
+r.start = [1, 2, 3, 4]
+r.end = [0xcc, 0xdd]
+r.group_id = group
+
+print n.read_data_range(r)
