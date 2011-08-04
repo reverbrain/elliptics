4998f3bba48470d0af520938b27fdb73b1c23c18
Anton Kortunov <toshic.toshic@gmail.com>
2011-08-04 10:29:22 +0400

    Process READ and WRITE commands for metadata

--- library/dnet.c
+++ library/dnet.c
@@ -681,7 +681,7 @@ int dnet_process_cmd_raw(struct dnet_net_state *st, struct dnet_cmd *cmd, void *
 				io = data;
 				dnet_convert_io_attr(io);
 
-				if (a->cmd == DNET_CMD_DEL) {
+				if (a->cmd == DNET_CMD_DEL || io->flags & DNET_IO_FLAGS_META) {
 					err = dnet_process_meta(st, cmd, a, data);
 					break;
 				}
