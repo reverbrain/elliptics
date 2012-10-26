2b9ee733a4e193b0a323960276326fea5ef9f97b
Evgeniy Polyakov <zbr@ioremap.net>
2012-10-26 22:51:32 +0400

    cmake tweaks

--- cmake/Modules/FindLevelDB.cmake
+++ cmake/Modules/FindLevelDB.cmake
@@ -6,10 +6,15 @@
 #  LEVELDB_INCLUDE_DIRS - the include path of the LevelDB library
 
 if (LEVELDB_INCLUDE_DIRS AND LEVELDB_LIBRARIES)
-    set (LEVELDB_FOUND TRUE)
+	message("leveldb: include: '${LEVELDB_INCLUDE_DIRS}', libs: '${LEVELDB_LIBRARIES}'")
+	set (LEVELDB_FOUND TRUE)
 else()
+	find_library(SNAPPY_LIBRARIES NAMES snappy PATHS ${SNAPPY_LIBRARY_DIRS})
 	find_library(LEVELDB_LIBRARIES NAMES leveldb PATHS ${LEVELDB_LIBRARY_DIRS})
-    find_path(LEVELDB_INCLUDE_DIRS NAMES leveldb/c.h PATHS)
+	list(APPEND LEVELDB_LIBRARIES ${SNAPPY_LIBRARIES})
+	find_path(LEVELDB_INCLUDE_DIRS NAMES leveldb/c.h PATHS)
+
+	message("leveldb:libs: ${LEVELDB_LIBRARIES}")
 endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LEVELDB DEFAULT_MSG LEVELDB_LIBRARIES LEVELDB_INCLUDE_DIRS)
