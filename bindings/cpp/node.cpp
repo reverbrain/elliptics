cc7ebed40200434f1e460025d67e97328a4f36d1
Evgeniy Polyakov <zbr@ioremap.net>
2012-11-14 20:16:07 +0400

    Minor cleanup

--- bindings/cpp/node.cpp
+++ bindings/cpp/node.cpp
@@ -959,12 +959,10 @@ std::vector<std::string> session::read_data_range(struct dnet_io_attr &io, int g
 
 					dnet_convert_io_attr(io);
 
-					uint64_t size = dnet_bswap64(io->size);
-
 					std::string str;
 
 					str.append((char *)io->id, DNET_ID_SIZE);
-					str.append((char *)&size, 8);
+					str.append((char *)&io->size, sizeof(io->size));
 					str.append((const char *)(io + 1), io->size);
 
 					ret.push_back(str);
