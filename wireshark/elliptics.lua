8b06121f6ecddf76d1c78028fdcc1ed1f5955e0e
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-02-08 20:09:31 +0400

    Added wireshark dissector

---
+++ wireshark/elliptics.lua
@@ -0,0 +1,130 @@
+  do
+	local elliptics_proto = Proto("elliptics", "Elliptics");
+	
+	
+	
+	function elliptics_proto.dissector(buffer, pinfo, tree)
+		pinfo.cols.protocol = "ELLIPTICS"
+		
+		local header_size = 104
+		local buf_len = buffer:len()
+		local offset = 0
+		
+		local elliptics_command = function(label, fields)
+			return {
+				["label"] = label,
+				["parse"] = function(buffer, off, tree)
+								return 1
+			                end
+			}
+		end
+		
+		local commands = {
+			[1] = elliptics_command(
+				"DNET_CMD_LOOKUP", {}
+			),
+			[2] = elliptics_command(
+				"DNET_CMD_REVERSE_LOOKUP", {}
+			),
+			[3] = elliptics_command(
+				"DNET_CMD_JOIN", {}
+			),
+			[4] = elliptics_command(
+				"DNET_CMD_WRITE", {}
+			),
+			[5] = elliptics_command(
+				"DNET_CMD_READ", {}
+			),
+			[6] = elliptics_command(
+				"DNET_CMD_LIST", {}
+			),
+			[7] = elliptics_command(
+				"DNET_CMD_EXEC", {}
+			),
+			[8] = elliptics_command(
+				"DNET_CMD_ROUTE_LIST", {}
+			),
+			[9] = elliptics_command(
+				"DNET_CMD_STAT", {}
+			),
+			[10] = elliptics_command(
+				"DNET_CMD_NOTIFY", {}
+			),
+			[11] = elliptics_command(
+				"DNET_CMD_DEL", {}
+			),
+			[12] = elliptics_command(
+				"DNET_CMD_STAT_COUNT", {}
+			),
+			[13] = elliptics_command(
+				"DNET_CMD_STATUS", {}
+			),
+			[14] = elliptics_command(
+				"DNET_CMD_READ_RANGE", {}
+			),
+			[15] = elliptics_command(
+				"DNET_CMD_DEL_RANGE", {}
+			),
+			[16] = elliptics_command(
+				"DNET_CMD_AUTH", {}
+			),
+			[17] = elliptics_command(
+				"DNET_CMD_BULK_READ", {}
+			),
+			[18] = elliptics_command(
+				"DNET_CMD_DEFRAG", {}
+			)
+		}
+		
+		local unknown_command = elliptics_command(
+			"DNET_CMD_UNKNOWN", {}
+		)
+		
+		while offset + header_size <= buf_len do
+			local data_size = buffer(96, 4):le_uint()
+			local packet_size = header_size + data_size
+			
+			if buf_len - offset < packet_size then
+				break
+			end
+			
+			local packet = buffer:range(offset, packet_size)
+			
+			local item = tree:add(elliptics_proto, packet(), "Elliptics CMD Header")
+			
+			local dnet_id = item:add(elliptics_proto, packet(0, 72), "dnet_id")
+			dnet_id:add(packet(0, 64), "id: " .. packet(0, 64))
+			dnet_id:add(packet(64, 4), "group_id: " .. packet(64, 4):le_uint())
+			dnet_id:add(packet(68, 4), "type: " .. packet(68, 4):le_uint())
+			
+			local cmd_id = packet(76, 4):le_uint()
+			local cmd = commands[cmd_id]
+			local cmd_label
+			if cmd == nil then
+				cmd = unknown_command
+			end
+			
+			item:add(packet(72, 4), "status: " .. packet(72, 4):le_uint())
+			item:add(packet(76, 4), "cmd: " .. cmd.label .. " (" .. cmd_id .. ")")
+			item:add(packet(80, 8), "flags: " .. packet(80, 8))
+			item:add(packet(88, 8), "trans: " .. packet(88, 8))
+			item:add(packet(96, 8), "size: " .. packet(96, 4):le_uint())
+			if data_size > 0 then
+				item:add(packet(104, data_size), "data: " .. packet(104, data_size))
+			end
+			
+			offset = offset + packet_size
+		end
+		
+		if offset ~= buf_len then
+			pinfo.desegment_len = DESEGMENT_ONE_MORE_SEGMENT
+			pinfo.desegment_offset = offset
+			return DESEGMENT_ONE_MORE_SEGMENT
+			end
+		
+		return buf_len
+	end
+
+	local tcp_table = DissectorTable.get("tcp.port")
+	tcp_table:add(1025, elliptics_proto)
+  end
