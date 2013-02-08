e40e32dc5c5daa3aea59c282491438e6ce556949
Denis Zaitsev <zaitsev.dennis@yandex.ru>
2013-02-08 21:03:43 +0400

    Some fixes for wireshark=1.2.7-1

--- wireshark/elliptics.lua
+++ wireshark/elliptics.lua
@@ -89,7 +89,7 @@
 			local item = tree:add(elliptics_proto, buffer(offset, packet_size), "Elliptics CMD Header")
 			
 			local dnet_id = item:add(elliptics_proto, buffer(offset + 0, 72), "dnet_id")
-			dnet_id:add(buffer(offset + 0, 64), "id: " .. buffer(offset + 0, 64))
+			dnet_id:add(buffer(offset + 0, 64), "id: " .. tostring(buffer(offset + 0, 64)))
 			dnet_id:add(buffer(offset + 64, 4), "group_id: " .. buffer(offset + 64, 4):le_uint())
 			dnet_id:add(buffer(offset + 68, 4), "type: " .. buffer(offset + 68, 4):le_uint())
 			
@@ -102,11 +102,11 @@
 			
 			item:add(buffer(offset + 72, 4), "status: " .. buffer(offset + 72, 4):le_uint())
 			item:add(buffer(offset + 76, 4), "cmd: " .. cmd.label .. " (" .. cmd_id .. ")")
-			item:add(buffer(offset + 80, 8), "flags: " .. buffer(offset + 80, 8))
-			item:add(buffer(offset + 88, 8), "trans: " .. buffer(offset + 88, 8))
+			item:add(buffer(offset + 80, 8), "flags: " .. tostring(buffer(offset + 80, 8)))
+			item:add(buffer(offset + 88, 8), "trans: " .. tostring(buffer(offset + 88, 8)))
 			item:add(buffer(offset + 96, 8), "size: " .. buffer(offset + 96, 4):le_uint())
 			if data_size > 0 then
-				item:add(buffer(offset + 104, data_size), "data: " .. buffer(offset + 104, data_size))
+				item:add(buffer(offset + 104, data_size), "data: " .. tostring(buffer(offset + 104, data_size)))
 			end
 			
 			offset = offset + packet_size
@@ -124,3 +124,4 @@
 	local tcp_table = DissectorTable.get("tcp.port")
 	tcp_table:add(1025, elliptics_proto)
   end
+
