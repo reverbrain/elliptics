e6cfb0693b5525a3b1e1b83f9245d256ec5cf15b
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-02-27 20:10:51 +0530

    ext: added flags to extension header

--- example/backends.h
+++ example/backends.h
@@ -88,7 +88,7 @@ struct dnet_config_backend {
 	struct dnet_backend_callbacks	cb;
 };
 
-/*! TODO: On-disk extensions header */
+/*! On-disk extensions header */
 struct dnet_ext_hdr {
 	uint32_t		etype;		/* Extension type */
 	uint32_t		size;		/* Size of data (excluding header) */
@@ -96,7 +96,7 @@ struct dnet_ext_hdr {
 	unsigned char		data[0];	/* Extension's payload */
 };
 
-/*! TODO: In-memory extensions */
+/*! In-memory extensions */
 struct dnet_ext {
 	uint32_t		etype;		/* Extension type */
 	uint32_t		size;		/* Size of data (excluding header) */
@@ -108,7 +108,8 @@ struct dnet_ext_list_hdr {
 	uint32_t		size;		/* Size of all extensions */
 	uint32_t		count;		/* Number of extensions in record */
 	struct dnet_time	timestamp;	/* Time stamp of record */
-	uint64_t		__pad[3];	/* For future use (should be NULLed) */
+	uint64_t		flags;		/* Custom flags for this record */
+	uint64_t		__pad[2];	/* For future use (should be NULLed) */
 };
 
 /*! In-memory extension conatiner */
@@ -116,6 +117,7 @@ struct dnet_ext_list {
 	uint32_t		size;		/* Total size of extensions */
 	uint32_t		count;		/* Number of entries in list */
 	struct dnet_time	timestamp;	/* TS of header */
+	uint64_t		flags;		/* Custom flags for this record */
 	struct dnet_ext		**exts;		/* Array of pointers to extensions */
 	void			*data;		/* Pointer to original data before extraction */
 };
