c6ad6c3bcd32144d08eb50676139acd731de7c36
Evgeniy Polyakov <zbr@ioremap.net>
2013-04-10 20:27:47 +0400

    Monor dnet_module_backend update

--- example/module_backend/module_backend.c
+++ example/module_backend/module_backend.c
@@ -5,44 +5,54 @@
 static void module_backend_cleanup(void *private_data)
 {
 	struct module_backend_t *module_backend = private_data;
+
 	module_backend->api->destroy_handler(module_backend->api);
+
 	destroy_dlopen_handle(&module_backend->dlopen_handle);
 	destroy_module_backend_config(&module_backend->config);
 }
 
 static int dnet_module_config_init(struct dnet_config_backend *b, struct dnet_config *c)
 {
-	int err;
 	struct module_backend_t *module_backend = b->data;
-	err=create_dlopen_handle(&module_backend->dlopen_handle, module_backend->config.module_path, module_backend->config.symbol_name);
-	if (0!=err) {
+	module_constructor* constructor;
+	int err;
+
+	err = create_dlopen_handle(&module_backend->dlopen_handle, module_backend->config.module_path, module_backend->config.symbol_name);
+	if (err) {
 		dnet_backend_log(DNET_LOG_ERROR, "Fail to create dlopen handle from %s\n", module_backend->config.module_path);
-		err=-ENOMEM;
-		goto err;
+		err = -ENOMEM;
+		goto err_out_exit;
 	}
-	module_constructor* constructor=module_backend->dlopen_handle.symbol;
-	module_backend->api=constructor(&module_backend->config);
-	if (NULL==module_backend->api) {
+
+	constructor = module_backend->dlopen_handle.symbol;
+	module_backend->api = constructor(&module_backend->config);
+	if (!module_backend->api) {
 		dnet_backend_log(DNET_LOG_ERROR, "Fail to create module_backend from %s\n", module_backend->config.module_path);
-		err=-ENOMEM;
-		goto constructor_err;
+		err = -ENOMEM;
+		goto err_out_constructor;
 	}
+
 	c->cb = &b->cb; /// @todo NOTE need to understand this is for.
 	b->cb.command_private = module_backend;
 	b->cb.command_handler = module_backend->api->command_handler;
 	b->cb.meta_write = module_backend->api->meta_write_handler;
-	b->cb.backend_cleanup=module_backend_cleanup;
+	b->cb.backend_cleanup = module_backend_cleanup;
+
 	dnet_backend_log(DNET_LOG_NOTICE, "Dynamic module_backend loaded successfully\n");
+
 	return 0;
-	constructor_err:
-		destroy_dlopen_handle(&module_backend->dlopen_handle);
-	err:
-		return err;
+
+err_out_constructor:
+	destroy_dlopen_handle(&module_backend->dlopen_handle);
+err_out_exit:
+	return err;
 }
 
 static void dnet_module_config_cleanup(struct dnet_config_backend *b)
 {
 	struct module_backend_t *module_backend = b->data;
+
 	module_backend_cleanup(module_backend);
 }
 
@@ -55,8 +65,9 @@ static struct dnet_config_backend dnet_module_backend = {
 
 int dnet_module_backend_init(void)
 {
-	dnet_module_backend.ent=dnet_config_entries_module();
-	dnet_module_backend.num=dnet_config_entries_module_size();
+	dnet_module_backend.ent = dnet_config_entries_module();
+	dnet_module_backend.num = dnet_config_entries_module_size();
+
 	return dnet_backend_register(&dnet_module_backend);
 }
 
