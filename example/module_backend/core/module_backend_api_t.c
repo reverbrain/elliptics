9c8858ba848278e01e6f78989003c76380a2b529
Ivan Tolstosheyev <itroot@yandex-team.ru>
2013-04-20 23:14:18 +0400

    Move source code to module_backend_t.c

--- example/module_backend/core/module_backend_api_t.c
+++
@@ -1,7 +0,0 @@
-#include <elliptics/module_backend.h>
-#include "../../backends.h"
-
-void report_module_backend_error(const char *what)
-{
-	dnet_backend_log(DNET_LOG_ERROR, "module_backend: failed: %s\n", what);
-}
