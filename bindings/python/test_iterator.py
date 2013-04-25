512552b44785460c3b65018766ee9b9a670e75a5
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-04-26 02:10:24 +0400

    test: rework iterator test, added flow control

--- bindings/python/test_iterator.py
+++ bindings/python/test_iterator.py
@@ -6,7 +6,7 @@ import sys
 sys.path.insert(0, "bindings/python/")
 import elliptics
 
-try:
+if __name__ == '__main__':
     log = elliptics.Logger("/dev/stderr", 1)
     cfg = elliptics.Config()
     cfg.cookie = "0123456789012345678901234567890123456789"
@@ -22,30 +22,31 @@ try:
     request.action = elliptics.iterator_actions.start
     request.itype = elliptics.iterator_types.network
     request.flags = elliptics.iterator_flags.key_range \
-        | elliptics.iterator_flags.ts_range | elliptics.iterator_flags.data
+            | elliptics.iterator_flags.ts_range | elliptics.iterator_flags.data
     request.key_begin = [0] * 64
     request.key_end = [255] * 64
     request.time_begin = elliptics.Time(0, 0)
     request.time_end = elliptics.Time(2**64-1, 2**64-1)
 
-    try:
-        eid = elliptics.Id([0] * 64, 2, 0)
-        iterator = s.start_iterator(eid, request)
-        for result in iterator:
-            try:
-                if result.status != 0:
-                    raise RuntimeError("Wrong status: {0}".format(result.status))
-                else:
-                    print "key: {0}, flags: {1}, ts: {2}/{3}, data: {4}".format(result.key,
-                            result.user_flags, result.timestamp.tsec, result.timestamp.tnsec,
-                            result.response_data())
-            except Exception as e:
-                print "Invalid element"
-                raise e
-    except Exception as e:
-        print "Iteration failed"
-        raise e
+    eid = elliptics.Id([0] * 64, 2, 0)
+    iterator = s.start_iterator(eid, request)
+    for i, result in enumerate(iterator):
+        if result.status != 0:
+            raise AssertionError("Wrong status: {0}".format(result.status))
 
-except Exception as e:
-    print "Unexpected error"
-    raise e
+        print "key: {0}, flags: {1}, ts: {2}/{3}, data: {4}".format(result.key,
+                result.user_flags, result.timestamp.tsec, result.timestamp.tnsec,
+                result.response_data())
+
+        # Test flow control
+        if i % 10 == 0:
+            print "Pause iterator"
+            pause = elliptics.IteratorRequest()
+            pause.id = result.id
+            pause.action = elliptics.iterator_actions.pause
+            pause_it = s.start_iterator(eid, pause)
+            print "Continue iterator"
+            cont = elliptics.IteratorRequest()
+            cont.id = result.id
+            cont.action = elliptics.iterator_actions.cont
+            cont_it = s.start_iterator(eid, cont)
