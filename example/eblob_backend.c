fdabd5631e4f914d8ff7058bae640433b083ef32
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-05-21 20:20:43 +0400

    ext: optimized blob_read()
    Previous implementaion had rather big overhead but had an advantage of
    being simple - it has single code path for compressed and non-compressed
    data.
    Now it's time to split this single codepath for more efficient
    implementaion of common case - read of non-compressed data.

--- example/eblob_backend.c
+++ example/eblob_backend.c
@@ -321,10 +321,10 @@ static int blob_read(struct eblob_backend_config *c, void *state, struct dnet_cm
 	struct eblob_backend *b = c->eblob;
 	struct eblob_key key;
 	struct eblob_write_control wc;
-	uint64_t offset, size = 0;
+	uint64_t offset = 0, size = 0;
 	char *read_data = NULL;
 	enum eblob_read_flavour csum = EBLOB_READ_CSUM;
-	int err, fd, free_data = 1, on_close = 0;
+	int err, fd = -1, free_data = 1, on_close = 0;
 
 	dnet_ext_list_init(&elist);
 	dnet_convert_io_attr(io);
@@ -335,11 +335,28 @@ static int blob_read(struct eblob_backend_config *c, void *state, struct dnet_cm
 		csum = EBLOB_READ_NOCSUM;
 
 	err = eblob_read_return(b, &key, io->type, csum, &wc);
-	if (err < 0) {
-		dnet_backend_log(DNET_LOG_ERROR, "%s: EBLOB: blob-read-fd: READ: %d: %s\n",
-			dnet_dump_id_str(io->id), err, strerror(-err));
-		goto err_out_exit;
-	} else {
+	if (err == 0) {
+		/* Existing entry */
+		offset = wc.data_offset;
+		size = wc.total_data_size;
+		fd = wc.data_fd;
+
+		/* Existing new-format entry */
+		if ((wc.flags & BLOB_DISK_CTL_USR1) != 0) {
+			struct dnet_ext_list_hdr ehdr;
+
+			err = dnet_ext_hdr_read(&ehdr, fd, offset);
+			if (err != 0)
+				goto err_out_free;
+			dnet_ext_hdr_to_list(&ehdr, &elist);
+			dnet_ext_list_to_io(&elist, io);
+
+			/* Take into an account extended header */
+			size -= sizeof(struct dnet_ext_list_hdr);
+			offset += sizeof(struct dnet_ext_list_hdr);
+		}
+	} else if (err > 0) {
+		/* Compressed entry */
 		err = eblob_read_data_nocsum(b, &key, io->offset, &read_data, &size, io->type);
 		if (err) {
 			dnet_backend_log(DNET_LOG_ERROR, "%s: EBLOB: blob-read-data: READ: %d: %s\n",
@@ -347,21 +364,21 @@ static int blob_read(struct eblob_backend_config *c, void *state, struct dnet_cm
 			goto err_out_exit;
 		}
 
-		offset = 0; /* to shut up compiler - offset is not used when there is data */
-		fd = -1;
-	}
+		/* Compressed entry new format */
+		if ((wc.flags & BLOB_DISK_CTL_USR1) != 0) {
+			err = dnet_ext_list_extract((void *)&read_data, (uint64_t *)&size,
+					&elist, DNET_EXT_FREE_ON_DESTROY);
+			if (err != 0)
+				goto err_out_free;
+			dnet_ext_list_to_io(&elist, io);
 
-	if ((wc.flags & BLOB_DISK_CTL_USR1) != 0) {
-		err = dnet_ext_list_extract((void *)&read_data, (uint64_t *)&size,
-				&elist, DNET_EXT_FREE_ON_DESTROY);
-		if (err != 0)
-			goto err_out_free;
-		/* It will be done by dnet_ext_list_destroy */
-		free_data = 0;
-	}
-
-	if (dnet_ext_list_to_io(&elist, io)) {
-		dnet_backend_log(DNET_LOG_ERROR, "%s: EBLOB: can't copy ext list to io.", dnet_dump_id_str(io->id));
+			/* It will be done by dnet_ext_list_destroy */
+			free_data = 0;
+		}
+	} else {
+		dnet_backend_log(DNET_LOG_ERROR, "%s: EBLOB: blob-read-fd: READ: %d: %s\n",
+			dnet_dump_id_str(io->id), err, strerror(-err));
+		goto err_out_exit;
 	}
 
 	io->size = size;
