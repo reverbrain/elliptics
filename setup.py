7e2b62724095786fe0dc77ad800d1b9db58e323e
Kirill Smorodinnikov <shaitkir@gmail.com>
2013-05-22 14:41:36 +0400

    Renamed recovery into dnet_recovery. Added recovery scripts to elliptics-client package.

--- setup.py
+++ setup.py
@@ -12,11 +12,22 @@ except:
 	pass
 
 print vstr
-setup(name='elliptics',
-      version=vstr,
-      description='Elliptics - client library for distributed storage system',
-      url='http://www.ioremap.net/projects/elliptics',
+setup(name = 'elliptics',
+      version = vstr,
+      description = 'Elliptics - client library for distributed storage system',
+      url = 'http://www.ioremap.net/projects/elliptics',
       package_dir = {'': 'bindings/python'},
-      py_modules=['elliptics'],
+      py_modules = ['elliptics'],
       license = 'GPLv2',
      )
+
+setup(name = 'elliptics_recovery',
+      version = vstr,
+      description = 'Elliptics - data center and merge recovery module',
+      url = 'http://www.ioremap.net/projects/elliptics',
+      package_dir = {'':'recovery', 'recover':'recovery/recover', 'recover/utils': 'recovery/recover/utils'},
+      py_modules = ['dc_recovery', 'recovery_merge'],
+      packages = ['recover', 'recover/utils'],
+      data_files = [('/usr/bin', ['recovery/dnet_recovery'])],
+      license = 'GPLv2',
+      )
