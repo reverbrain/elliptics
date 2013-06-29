c04b5487eaed53dbc5ca528957058ebc4462cf0e
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-06-29 04:04:23 +0400

    blob: fixed read with offset/size set
    Reported by: elessar@

--- example/eblob_backend.c
+++ example/eblob_backend.c
@@ -339,7 +339,20 @@ static int blob_read(struct eblob_backend_config *c, void *state, struct dnet_cm
 		goto err_out_exit;
 	}
 
-	io->size = size;
+	if (io->offset) {
+		if (io->offset >= size) {
+			err = -E2BIG;
+			goto err_out_exit;
+		}
+		offset += io->offset;
+		size -= io->offset;
+	}
+
+	if (io->size != 0 && size > io->size)
+		size = io->size;
+	else
+		io->size = size;
+
 	if (size && last)
 		cmd->flags &= ~DNET_FLAGS_NEED_ACK;
 
