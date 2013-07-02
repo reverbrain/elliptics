e124d8328ba71a4d951e200852ab9683d7e4b11c
Evgeniy Polyakov <zbr@ioremap.net>
2013-07-03 00:23:38 +0400

    Added percentage dump, help and other params to route table stats script

--- bindings/python/routes.py
+++ bindings/python/routes.py
@@ -5,6 +5,11 @@ import sys
 sys.path.append('bindings/python/')
 import elliptics
 
+from operator import itemgetter
+from pprint import pprint
+
+import argparse
+
 def sid(id, count=6):
 	ba = bytearray(id[0:count])
 	ret = ''
@@ -13,33 +18,96 @@ def sid(id, count=6):
 
 	return ret
 
-def main(remote = None):
-	log = elliptics.Logger("/dev/stderr", 1)
-	n = elliptics.Node(log)
+def percentage(routes):
+    groups = {}
+    for r in routes:
+        if not r[0].group_id in groups:
+            groups[r[0].group_id] = []
 
-	s = elliptics.Session(n)
-	s.add_groups([1])
+        group = groups[r[0].group_id]
+        group.append((sid(r[0].id, 16), r[1]))
 
-	remotes = [("localhost", 1025), ]
-	if remote:
-		remotes = []
-		for r in remote:
-			spl = r.split(":")
-			remotes.append((spl[0], int(spl[1])))
+    groups_percent = {}
+    for g in groups.iterkeys():
+        #print "routes for group %d" % g
+        groups_percent[g] = {}
 
-	for r in remotes:
-		try:
-			n.add_remote(r[0], r[1])
-		except Exception as e:
-			pass
+        host = groups[g][-1][1]
+        groups[g].insert(0, ('0'*32, host))
 
-	routes = s.get_routes()
-	for r in sorted(routes, key=lambda eid_tuple: eid_tuple[0].id):
-		print r[0].group_id, sid(r[0].id), r[1]
+        #host = groups[g][1][1]
+        #groups[g].append(('f'*32, host))
+
+        groups[g].sort(key=itemgetter(0))
+
+        #for r in groups[g]:
+            #print '    %s %s' % (r[0], r[1])
+
+        host = groups[g][0][1]
+        start = 0
+        end = 0
+
+        for r in groups[g]:
+            #print 'host: %s, r[1]: %s' % (host, r[1])
+
+            if r[1] == host:
+                #print 'Skipping'
+                continue
+
+            end = int(r[0], 16)
+
+            if not host in groups_percent[g]:
+                groups_percent[g][host] = end-start
+            else:
+                groups_percent[g][host] += end-start
+
+            #print 'groups_percent[%d][%s] = %x' % (g, host, groups_percent[g][host])
+            host = r[1]
+            start = end
+
+        end = int('f'*32, 16)
+
+        if not host in groups_percent[g]:
+            groups_percent[g][host] = end-start
+        else:
+            groups_percent[g][host] += end-start
+
+        #print 'groups_percent[%d][%s] = %x' % (g, host, groups_percent[g][host])
+
+    for g in groups_percent:
+        print 'Group: %d' % (g)
+        for h in groups_percent[g]:
+            print 'host %s %d%%' % (h, groups_percent[g][h] * 100 / int('f'*32, 16))
 
 if __name__ == '__main__':
-	remote = None
-	if len(sys.argv) > 1:
-		remote = sys.argv[1:]
+	parser = argparse.ArgumentParser(description='Get remote route table and print its statistics.')
+	parser.add_argument('remotes', metavar='N', type=str, nargs='+',
+		help='Remote nodes to connect and grab route tables. Format: addr:port:family, where family = 2 for ipv4 and 10 for ipv6')
+	parser.add_argument('--percentage', dest='percentage', action='store_true',
+		help='if present, dump parts of DHT ring each node occupies (in percents)')
+	parser.add_argument('--log', default='/dev/stdout', help='log file')
+	parser.add_argument('--log-level', type=int, default=elliptics.log_level.error,
+		help='log level: %d-%d' % (elliptics.log_level.data, elliptics.log_level.debug))
 
-	main(remote)
+	args = parser.parse_args()
+	if len(args.remotes) == 0:
+		args.remotes = "localhost:1025:2"
+
+	log = elliptics.Logger(args.log, args.log_level)
+	n = elliptics.Node(log)
+	s = elliptics.Session(n)
+
+	try:
+		for r in args.remotes:
+			spl = r.split(":")
+			n.add_remote(addr=spl[0], port=int(spl[1]), family=int(spl[2]))
+	except Exception as e:
+		print e
+		pass
+
+	routes = s.get_routes()
+	if args.percentage:
+		percentage(routes)
+	else:
+		for r in sorted(routes, key=lambda eid_tuple: eid_tuple[0].id):
+			print r[0].group_id, sid(r[0].id), r[1]
