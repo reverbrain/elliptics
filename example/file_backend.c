6d05c6ceb04465a37d07c8def39d24c4cf1bd7c3
Alexey Ivanov <SaveTheRbtz@GMail.com>
2013-07-10 05:28:08 +0400

    fs: use EBLOB_AUTO_DATASORT for metadata blob

--- example/file_backend.c
+++ example/file_backend.c
@@ -556,7 +556,7 @@ static int dnet_file_db_init(struct file_backend_root *r, struct dnet_config *c,
 	memset(&ecfg, 0, sizeof(ecfg));
 	ecfg.file = meta_path;
 	ecfg.sync = r->sync;
-	ecfg.blob_flags = EBLOB_NO_FREE_SPACE_CHECK;
+	ecfg.blob_flags = EBLOB_NO_FREE_SPACE_CHECK | EBLOB_AUTO_DATASORT;
 	ecfg.records_in_blob = r->records_in_blob;
 	ecfg.blob_size = r->blob_size;
 	ecfg.defrag_percentage = r->defrag_percentage;
