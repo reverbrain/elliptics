91b1f0c5536c29954abfab12787d70435fa9d5fc
Kirill Smorodinnikov <shaitkir@gmail.com>
2013-10-07 04:49:41 +0400

    Python: added copyrights

--- bindings/python/src/__init__.py
+++ bindings/python/src/__init__.py
@@ -7,6 +7,25 @@ from elliptics.route import *
 from elliptics.session import *
 from elliptics.node import *
 
+__author__ = "Kirill Smorodinnikov, Evgeniy Polyakov, Ruslan Nigmatullin, Alexey Ivanov"
+__copyright__ = """2008+ Copyright (c) Evgeniy Polyakov <zbr@ioremap.net>
+2012+ Copyright (c) Ruslan Nigmatullin <euroelessar@yandex.ru>
+2013+ Copyright (c) Kirill Smorodinnikov <shaitkir@gmail.com>
+All rights reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details."""
+__credits__ = ["Kirill Smorodinnikov", "Evgeniy Polyakov", "Ruslan Nigmatullin", "Alexey Ivanov"]
+__license__ = "GPLv2"
+__maintainer__ = "Kirill Smorodinnikov"
+
 
 @property
 def storage_address(self):
