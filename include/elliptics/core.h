2c3300d364d981b2d1bf0c606e6ecd9cef1fd77e
Kirill Smorodinnikov <shaitkir@gmail.com>
2013-10-24 22:33:54 +0400

    Core: renamed offsetof to dnet_offsetof.

--- include/elliptics/core.h
+++ include/elliptics/core.h
@@ -74,11 +74,11 @@
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
-#ifndef offsetof
+#ifndef dnet_offsetof
 #ifdef __compiler_offsetof
-#define offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
+#define dnet_offsetof(TYPE,MEMBER) __compiler_offsetof(TYPE,MEMBER)
 #else
-#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#define dnet_offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 #endif
 #endif
 
