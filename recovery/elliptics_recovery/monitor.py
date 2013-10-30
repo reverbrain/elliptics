49a79eeab03d7904973363b191674c4731aebc71
Kirill Smorodinnikov <shaitkir@gmail.com>
2013-10-30 06:01:31 +0400

    License: Updated Copyright.

--- recovery/elliptics_recovery/monitor.py
+++ recovery/elliptics_recovery/monitor.py
@@ -1,3 +1,19 @@
+# =============================================================================
+# 2013+ Copyright (c) Kirill Smorodinnikov <shaitkir@gmail.com>
+# 2013+ Copyright (c) Alexey Ivanov <rbtz@ph34r.me>
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# =============================================================================
+
 """
 Wrapper for monitoring data and working with it in user code
 """
