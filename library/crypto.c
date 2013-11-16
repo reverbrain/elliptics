d8d045b62b28a4e98b78091175518d84e8d02286
Ruslan Nigmatullin <euroelessar@yandex.ru>
2013-11-16 21:12:26 +0400

    License: Change license from GPL to LGPL

--- library/crypto.c
+++ library/crypto.c
@@ -14,7 +14,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with Elliptics.  If not, see <http://www.gnu.org/licenses/>.
  */
 
