42431d6f96fbe7ef6ff49e6984a4621461bb3739
Evgeniy Polyakov <zbr@ioremap.net>
2014-08-15 04:05:27 +0400

    procfs-provider: do not use backend logger, directly use common logger. The former requires linking with yet unavalable libelliptics

--- monitor/procfs_provider.cpp
+++ monitor/procfs_provider.cpp
@@ -43,7 +43,7 @@ static int fill_proc_io_stat(dnet_logger *l, struct proc_io_stat &st) {
 	f = fopen("/proc/self/io", "r");
 	if (!f) {
 		err = -errno;
-		dnet_backend_log(l, DNET_LOG_ERROR, "Failed to open '/proc/self/io': %s [%d].",
+		dnet_log_only_log(l, DNET_LOG_ERROR, "Failed to open '/proc/self/io': %s [%d].",
 		                 strerror(errno), errno);
 		goto err_out_exit;
 	}
@@ -83,7 +83,7 @@ static int fill_proc_stat(dnet_logger *l, struct proc_stat &st) {
 	f = fopen("/proc/self/stat", "r");
 	if (!f) {
 		err = -errno;
-		dnet_backend_log(l, DNET_LOG_ERROR, "Failed to open '/proc/self/stat': %s [%d].",
+		dnet_log_only_log(l, DNET_LOG_ERROR, "Failed to open '/proc/self/stat': %s [%d].",
 		                 strerror(errno), errno);
 		goto err_out_exit;
 	}
@@ -96,7 +96,7 @@ static int fill_proc_stat(dnet_logger *l, struct proc_stat &st) {
 	f = fopen("/proc/self/statm", "r");
 	if (!f) {
 		err = -errno;
-		dnet_backend_log(l, DNET_LOG_ERROR, "Failed to open '/proc/self/statm': %s [%d].",
+		dnet_log_only_log(l, DNET_LOG_ERROR, "Failed to open '/proc/self/statm': %s [%d].",
 		                 strerror(errno), errno);
 		goto err_out_exit;
 	}
