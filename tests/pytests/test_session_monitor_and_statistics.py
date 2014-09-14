95871ce63ad84c04c5a734f4f896d5d7b315b704
Evgeniy Polyakov <zbr@ioremap.net>
2014-09-14 05:59:45 +0400

    pytest: put utf8 encoding hint into monitor/stats test

--- tests/pytests/test_session_monitor_and_statistics.py
+++ tests/pytests/test_session_monitor_and_statistics.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # =============================================================================
 # 2013+ Copyright (c) Kirill Smorodinnikov <shaitkir@gmail.com>
