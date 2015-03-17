470ba9df7ab1060717b72bc21c2b16d31f1f948b
abudnik <budnik27@gmail.com>
2015-03-17 18:27:13 +0300

    refactored tests for event statistics

--- tests/stats_test.cpp
+++ tests/stats_test.cpp
@@ -146,7 +146,71 @@ static void test_top_statistics_existence()
  ****************/
 typedef ioremap::monitor::event_stats<test_event> stats_t;
 
-static void test_event_stats_boundary_conditions()
+static void test_empty_top()
+{
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result;
+
+	stats.get_top(TOP_LENGTH, default_time, result);
+	BOOST_CHECK_MESSAGE(result.empty(), "get_top must return empty list, if no events were added");
+}
+
+static void test_top_list_result_limit()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result;
+
+	// insert different items at the same time and with the same weight
+	for(int i = 0; i < 2 * TOP_LENGTH; ++i) {
+		test_event e{std::to_string(static_cast<long long>(i)), default_size, 1., default_time};
+		stats.add_event(e, e.get_time());
+	}
+
+	stats.get_top(TOP_LENGTH, default_time, result);
+	BOOST_REQUIRE_MESSAGE(result.size() == TOP_LENGTH, "get_top must return no more events than was requested");
+}
+
+static void test_top_list_result_boundary()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result;
+
+	const int few_events = 5;
+	BOOST_CHECK_MESSAGE(TOP_LENGTH > few_events, "it is necessary to request more events than were added");
+
+	// insert different items at the same time and with the same weight
+	for(int i = 0; i < few_events; ++i) {
+		test_event e{std::to_string(static_cast<long long>(i)), default_size, 1., default_time};
+		stats.add_event(e, e.get_time());
+	}
+
+	stats.get_top(TOP_LENGTH, default_time, result);
+	BOOST_REQUIRE_MESSAGE(result.size() == few_events, "get_top should not return more events, than were added");
+}
+
+static void test_top_list_capacity_limit()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result;
+
+	// insert different items at the same time and with the same weight
+	for(int i = 0; i < 4 * EVENTS_LIMIT; ++i) {
+		test_event e{std::to_string(static_cast<long long>(rand())), default_size, 1., default_time};
+		stats.add_event(e, e.get_time());
+	}
+
+	stats.get_top(4*EVENTS_LIMIT, default_time, result);
+	BOOST_REQUIRE_MESSAGE(result.size() == EVENTS_LIMIT, "get_top must return no more events than EVENTS_LIMIT");
+}
+
+static void test_all_events_expiration()
 {
 	const size_t default_size = 100;
 	const time_t default_time = time(nullptr);
@@ -154,45 +218,14 @@ static void test_event_stats_boundary_conditions()
 	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
 	std::vector<test_event> result;
 
-	// get_top returns empty list, if there was no added events
-	stats.get_top(TOP_LENGTH, default_time, result);
-	BOOST_CHECK(result.empty());
-
-	// get_top returns no more events than was requested
-	for(int i = 0; i < 2 * TOP_LENGTH; ++i) {
-		test_event e{std::to_string(static_cast<long long>(i)), default_size, 1., default_time};
-		stats.add_event(e, e.get_time());
-	}
-
-	stats.get_top(TOP_LENGTH, default_time, result);
-	BOOST_REQUIRE_EQUAL(result.size(), TOP_LENGTH);
-
-	// get_top returns no more events than EVENTS_LIMIT
-	for(int i = 0; i < 4 * EVENTS_LIMIT; ++i) {
+	// insert different items at the same time and with the same weight
+	for(int i = 0; i < EVENTS_LIMIT; ++i) {
 		test_event e{std::to_string(static_cast<long long>(rand())), default_size, 1., default_time};
 		stats.add_event(e, e.get_time());
 	}
 
-	result.clear();
-	stats.get_top(4*EVENTS_LIMIT, default_time, result);
-	BOOST_REQUIRE_EQUAL(result.size(), EVENTS_LIMIT);
-
-	// all events has been expired since expire_time elapsed
-	result.clear();
 	stats.get_top(TOP_LENGTH, expire_time, result);
-	BOOST_CHECK(result.empty());
-
-	// get_top should not return more events, than were added
-	const int few_events = 5;
-	BOOST_CHECK(few_events < TOP_LENGTH);
-
-	for(int i = 0; i < few_events; ++i) {
-		test_event e{std::to_string(static_cast<long long>(i)), default_size, 1., default_time};
-		stats.add_event(e, e.get_time());
-	}
-
-	stats.get_top(TOP_LENGTH, default_time, result);
-	BOOST_REQUIRE_EQUAL(result.size(), few_events);
+	BOOST_CHECK_MESSAGE(result.empty(), "all events must be expired since expire_time elapsed");
 }
 
 template<typename container_t>
@@ -208,29 +241,33 @@ static size_t events_symmetric_diff(container_t &fst, container_t &snd)
 	return std::distance(diff.begin(), it);
 }
 
-/*
- * Check max-heap property of event_stats. All events inserted at the same time.
- * All events describe access to different keys.
- */
-static void test_event_stats_no_time_dependency()
+static void test_same_event_many_insertions()
 {
 	const size_t default_size = 100;
 	const time_t default_time = time(nullptr);
 	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
-	stats_t stats_rand(EVENTS_SIZE, PERIOD_IN_SECONDS);
 	std::vector<test_event> result;
 
+	// insert same items at the same time and with the same weight
 	for(int i = 0; i < TOP_LENGTH; ++i) {
 		test_event e{"same", default_size, 1., default_time};
 		stats.add_event(e, e.get_time());
 	}
 
 	stats.get_top(TOP_LENGTH, default_time, result);
-	BOOST_REQUIRE_EQUAL(result.size(), 1);
-	BOOST_REQUIRE_EQUAL(result.back().get_weight(), TOP_LENGTH * default_size);
+	BOOST_REQUIRE_MESSAGE(result.size() == 1, "one and only one element must be in top");
+	BOOST_REQUIRE_MESSAGE(result.back().get_weight() == TOP_LENGTH * default_size,
+			      "event total weight must be incremented every time element inserted");
+}
 
-	// monotonically increment event's weight within events_limit
-	// last added events are heavies, so they must appear in get_top
+static void test_insertion_with_increasing_weight()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result;
+
+	// monotonically increment event's weight
 	std::vector<test_event> top_events;
 	for(int i = 1; i <= 3 * TOP_LENGTH; ++i) {
 		test_event e{std::to_string(static_cast<long long>(i)), i * default_size, 1., default_time};
@@ -239,13 +276,19 @@ static void test_event_stats_no_time_dependency()
 		stats.add_event(e, e.get_time());
 	}
 
-	result.clear();
 	stats.get_top(TOP_LENGTH, default_time, result);
 	BOOST_REQUIRE_EQUAL(result.size(), TOP_LENGTH);
-	BOOST_REQUIRE_EQUAL(events_symmetric_diff(top_events, result), 0);
+	BOOST_REQUIRE_MESSAGE(events_symmetric_diff(top_events, result) == 0,
+			      "last added events are heaviest => they must appear in get_top result");
+}
 
-	// generate events with random weights within events_limit
-	// use min_heap to find top (heaviest) events
+static void test_insertion_with_random_weight()
+{
+	const time_t default_time = time(nullptr);
+	stats_t stats_rand(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result;
+
+	// generate events with random weights, use min_heap to find top (heaviest) events
 	std::vector<test_event> min_heap;
 	min_heap.reserve(TOP_LENGTH);
 	std::function<decltype(test_event::weight_compare)> comparator_weight(&test_event::weight_compare);
@@ -264,13 +307,18 @@ static void test_event_stats_no_time_dependency()
 		stats_rand.add_event(e, e.get_time());
 	}
 
-	result.clear();
 	stats_rand.get_top(TOP_LENGTH, default_time, result);
 	BOOST_REQUIRE_EQUAL(result.size(), TOP_LENGTH);
-	BOOST_REQUIRE_EQUAL(events_symmetric_diff(min_heap, result), 0);
+	BOOST_REQUIRE_MESSAGE(events_symmetric_diff(min_heap, result) == 0,
+			      "heaviest events must be in top list");
+}
+
+static void test_event_insertion_order_independence()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
 
 	// check that statistics doesn't depend on order of key insertion
-	// order of insertion should not impact on get_top results
 	std::vector<test_event> test_set;
 	for(int i = 1; i < 8; ++i) {
 		test_event e{std::to_string(static_cast<long long>(i)), i * default_size, 1., default_time};
@@ -278,34 +326,29 @@ static void test_event_stats_no_time_dependency()
 	}
 
 	do {
-		std::vector<test_event> permut(test_set);
 		stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+		std::vector<test_event> result;
+		std::vector<test_event> permut(test_set);
 		for(auto it = permut.cbegin(); it != permut.cend(); ++it) {
 			stats.add_event(*it, it->get_time());
 		}
-		result.clear();
 		stats.get_top(TOP_LENGTH, default_time, result);
-		BOOST_REQUIRE_EQUAL(result.size(), permut.size());
-		BOOST_REQUIRE_EQUAL(events_symmetric_diff(result, permut), 0);
+		BOOST_REQUIRE_MESSAGE(result.size() == permut.size(),
+				    "result must contain all inserted events");
+		BOOST_REQUIRE_MESSAGE(events_symmetric_diff(result, permut) == 0,
+				      "order of insertion should not impact on get_top results");
 	} while (next_permutation(test_set.begin(), test_set.end(), test_event::key_compare_event));
 }
 
-/*
- * Check max-heap property of event_stats.
- */
-static void test_event_stats_with_time_dependency()
+static void test_event_weight_attenuation()
 {
 	const size_t default_size = 100;
 	const time_t default_time = time(nullptr);
 	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
-	stats_t stats2(EVENTS_SIZE, PERIOD_IN_SECONDS);
-	stats_t stats3(EVENTS_SIZE, PERIOD_IN_SECONDS);
-	std::vector<test_event> result, top_events;
+	std::vector<test_event> result;
 
-	// all events describe access to the same key, but at different period of time during
-	// time window much larger than PERIOD_IN_SECONDS.
-	// Event weight increments all the time inside PERIOD_IN_SECONDS time window, but
-	// old events (outside observable period of time) are omitted
+	// all events describe access to the same key with the same size, but at different period of time
+	// during time window much larger than PERIOD_IN_SECONDS.
 	const size_t long_period = 10 * PERIOD_IN_SECONDS;
 	for(size_t i = 0; i < long_period; ++i) {
 		test_event e{"same", default_size, 1., default_time + static_cast<time_t>(i)};
@@ -313,43 +356,67 @@ static void test_event_stats_with_time_dependency()
 	}
 
 	stats.get_top(TOP_LENGTH, default_time + long_period, result);
-	BOOST_REQUIRE_EQUAL(result.size(), 1);
-	BOOST_CHECK(result.back().get_weight() <= PERIOD_IN_SECONDS * default_size);
+	BOOST_REQUIRE_MESSAGE(result.size() == 1, "one and only one element must be in top");
+	BOOST_CHECK_MESSAGE(result.back().get_weight() <= PERIOD_IN_SECONDS * default_size,
+			    "old events (outside observable period of time) shouldn't impact on event weight");
 
-	// Added single event to the "same" key
-	// after PERIOD_IN_SECONDS / 2 since last event were added (in previous test).
-	// Weight of event must be proportionally smaller after significant period of silence,
-	// because older events are outdated (outside time window)
+	// Added single event with the "same" key
+	// after PERIOD_IN_SECONDS / 2 (half window length) since last event were added in previous test.
 	test_event e{"same", default_size, 1., default_time + static_cast<time_t>(long_period + PERIOD_IN_SECONDS / 2)};
 	stats.add_event(e, e.get_time());
 	result.clear();
 	stats.get_top(TOP_LENGTH, e.get_time(), result);
-	BOOST_REQUIRE_EQUAL(result.size(), 1);
-	BOOST_CHECK(result.back().get_weight() <= PERIOD_IN_SECONDS * default_size / 2);
+	BOOST_CHECK_MESSAGE(result.back().get_weight() <= PERIOD_IN_SECONDS * default_size / 2,
+			    "weight of event must be proportionally smaller after significant period of silence, "
+			    "because older events are outdated (outside time window)");
+}
 
-	// small key "sum" with regular frequent access must popup in top stats among heavier keys with single access
+static void test_frequent_access_among_heavy_keys()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result, top_events;
+	std::string key;
+	size_t size;
+
+	// half of inserted events reflects access to the "same" single key,
+	// other half of events reflects access to different keys.
 	for(int i = 0; i < 3 * TOP_LENGTH; ++i) {
-		size_t size = i % 2 ? default_size : default_size * 10;
-		std::string key = i % 2 ? std::string("sum") : std::to_string(static_cast<long long>(i));
+		if (i % 2) {
+			key = std::string("same");
+			size = default_size;
+		} else {
+			key = std::to_string(static_cast<long long>(i));
+			size = default_size * 10;
+		}
 
 		test_event e{key, size, 1., default_time + i};
-		stats2.add_event(e, e.get_time());
+		stats.add_event(e, e.get_time());
 	}
 
-	result.clear();
-	stats2.get_top(TOP_LENGTH, default_time + 3 * TOP_LENGTH, result);
+	stats.get_top(TOP_LENGTH, default_time + 3 * TOP_LENGTH, result);
 	BOOST_REQUIRE_EQUAL(result.size(), TOP_LENGTH);
-	BOOST_REQUIRE_EQUAL(*result.front().get_key(), "sum");
-	BOOST_CHECK(result.front().get_weight() > result.back().get_weight());
-	BOOST_CHECK(result.front().get_frequency() > result.back().get_frequency());
+	BOOST_REQUIRE_MESSAGE(*result.front().get_key() == "same",
+			      "key 'same' with regular frequent access must be first in top list");
+	BOOST_CHECK_MESSAGE(result.front().get_frequency() > result.back().get_frequency(),
+			    "key 'same' frequency must be greater then any other key");
+}
 
-	// if keys have same size and time access pattern, then keys with more frequent access must be in top
+static void test_frequent_access()
+{
+	const size_t default_size = 100;
+	const time_t default_time = time(nullptr);
+	stats_t stats(EVENTS_SIZE, PERIOD_IN_SECONDS);
+	std::vector<test_event> result, top_events;
+
+	// all inserted events have same size
 	for(int i = 0; i < 5 * TOP_LENGTH; ++i) {
 		std::string key = std::to_string(static_cast<long long>(i));
-		// access to i-th key occurs i times
+		// access to i-th key occurs i+1 times
 		for(int j = 0; j < i+1; ++j) {
 			test_event e{key, default_size, 1., default_time + j};
-			stats3.add_event(e, e.get_time());
+			stats.add_event(e, e.get_time());
 		}
 		if (i >= 4 * TOP_LENGTH) {
 			test_event e{key, default_size, 1., default_time};
@@ -357,19 +424,28 @@ static void test_event_stats_with_time_dependency()
 		}
 	}
 
-	result.clear();
-	stats3.get_top(TOP_LENGTH, default_time + 5 * TOP_LENGTH, result);
+	stats.get_top(TOP_LENGTH, default_time + 5 * TOP_LENGTH, result);
 	BOOST_REQUIRE_EQUAL(result.size(), TOP_LENGTH);
-	BOOST_REQUIRE_EQUAL(events_symmetric_diff(top_events, result), 0);
+	BOOST_REQUIRE_MESSAGE(events_symmetric_diff(top_events, result) == 0,
+			      "if keys have same size and time access pattern, "
+			      "then keys with more frequent access must be in top");
 }
 
-bool register_tests(test_suite *suite, node n)
+bool register_tests(test_suite *suite)
 {
-	(void) n;
 	ELLIPTICS_TEST_CASE_NOARGS(test_top_statistics_existence);
-	ELLIPTICS_TEST_CASE_NOARGS(test_event_stats_boundary_conditions);
-	ELLIPTICS_TEST_CASE_NOARGS(test_event_stats_no_time_dependency);
-	ELLIPTICS_TEST_CASE_NOARGS(test_event_stats_with_time_dependency);
+	ELLIPTICS_TEST_CASE_NOARGS(test_empty_top);
+	ELLIPTICS_TEST_CASE_NOARGS(test_top_list_result_limit);
+	ELLIPTICS_TEST_CASE_NOARGS(test_top_list_result_boundary);
+	ELLIPTICS_TEST_CASE_NOARGS(test_top_list_capacity_limit);
+	ELLIPTICS_TEST_CASE_NOARGS(test_all_events_expiration);
+	ELLIPTICS_TEST_CASE_NOARGS(test_same_event_many_insertions);
+	ELLIPTICS_TEST_CASE_NOARGS(test_insertion_with_increasing_weight);
+	ELLIPTICS_TEST_CASE_NOARGS(test_insertion_with_random_weight);
+	ELLIPTICS_TEST_CASE_NOARGS(test_event_insertion_order_independence);
+	ELLIPTICS_TEST_CASE_NOARGS(test_event_weight_attenuation);
+	ELLIPTICS_TEST_CASE_NOARGS(test_frequent_access_among_heavy_keys);
+	ELLIPTICS_TEST_CASE_NOARGS(test_frequent_access);
 
 	return true;
 }
@@ -396,11 +472,10 @@ boost::unit_test::test_suite *register_tests(int argc, char *argv[])
 		return NULL;
 	}
 
-	test_suite *suite = new test_suite("Local Test Suite");
-
 	configure_nodes(path);
 
-	register_tests(suite, *global_data->node);
+	test_suite *suite = new test_suite("event statistics test suite");
+	register_tests(suite);
 
 	return suite;
 }
