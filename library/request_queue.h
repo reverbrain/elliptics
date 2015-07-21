103b5ef958b80de27907cbb4c69f2f30e3186853
abudnik <budnik27@gmail.com>
2015-07-21 18:16:36 +0300

    request queue: added comments for dnet_request_queue class

--- library/request_queue.h
+++ library/request_queue.h
@@ -40,26 +40,67 @@ struct dnet_locks_entry
 	std::condition_variable unlock_event;
 };
 
+/*
+ * dnet_request_queue is queue of requests with specific key locking semantics: its pop_request()
+ * lookups first request with non-locked key in queue, locks this key and returns the request.
+ * Also it provides methods for specific key lock/unlock mechanism and provides internal statistics.
+ */
 class dnet_request_queue
 {
 public:
+	/*!
+	 * Constructor: initializes internal state properly
+	 */
 	dnet_request_queue();
+	/*!
+	 * Destructor: frees all dnet_locks_entry objects in /a m_lock_pool and destroys all requests in /a m_queue
+	 */
 	~dnet_request_queue();
 
+	/*!
+	 * Puts request \a req into /a m_queue
+	 */
 	void push_request(dnet_io_req *req);
-
+	/*!
+	 * Tries to take first available request with non-locked key and removes it from /a m_queue
+	 */
 	dnet_io_req *pop_request(dnet_work_io *wio, const char *thread_stat_id);
+	/*!
+	 * Releases request's /a req key from /a m_locked_keys
+	 */
 	void release_request(const dnet_io_req *req);
 
+	/*!
+	 * Saves key identified by /a id into /a m_locked_keys or waits until key will be unlocked (by calling release_request() or unlock_key())
+	 * and signalized using conditional var of dnet_locks_entry object associated with this key in /a m_locked_keys map.
+	 */
 	void lock_key(const dnet_id *id);
+	/*!
+	 * Removes key identified by /a id from /a m_locked_keys and notifies waiting threads
+	 */
 	void unlock_key(const dnet_id *id);
 
+	/*!
+	 * Returns internal queue statistics
+	 */
 	void get_list_stats(struct list_stat *stats) const;
 
 private:
+	/*
+	 * Returns first available request with non-locked key from /a m_queue and saves request's key into /a m_locked_keys
+	 */
 	dnet_io_req *take_request(dnet_work_io *wio, const char *thread_stat_id);
+	/*!
+	 * Removes key identified by /a id from /a m_locked_keys
+	 */
 	void release_key(const dnet_id *id);
+	/*!
+	 * Takes dnet_locks_entry object from /a m_lock_pool
+	 */
 	dnet_locks_entry *take_lock_entry();
+	/*!
+	 * Puts back dnet_locks_entry into /a m_lock_pool
+	 */
 	void put_lock_entry(dnet_locks_entry *entry);
 
 private:
